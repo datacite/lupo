2cafea04e69bc03826e1f93efb6dfd307074210e
Kristian Garza <kj.garza@gmail.com>
2017-07-27 19:51:31 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :heartbeat, only: [:index]
   resources :index, path: '/', only: [:index]
-  resources :datacentres, path: '/id/datacentres', constraints: { :id => /.+/ }
-  resources :datasets, path: '/id/datasets', constraints: { :id => /.+/ }
-  resources :prefixes, path: '/id/prefixes', constraints: { :id => /.+/ }
-  resources :allocators, path: '/id/allocators'
-  resources :members, path: '/id/members', controller: 'allocators'
-  resources :datacenters, path: '/id/datacenters', controller: 'datacentres'
+  resources :datacentres, constraints: { :id => /.+/ }
+  resources :datasets, constraints: { :id => /.+/ }
+  resources :prefixes, constraints: { :id => /.+/ }
+  resources :allocators
+  resources :members, controller: 'allocators'
+  resources :datacenters, controller: 'datacentres'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
