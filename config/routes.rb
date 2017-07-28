88b3f3d94118b09722a791235a51f1f36c144756
Kristian Garza <kj.garza@gmail.com>
2017-07-28 15:38:28 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :heartbeat, only: [:index]
-  resources :index, path: '/', only: [:index]
-  resources :datacentres, constraints: { :id => /.+/ }
-  resources :datasets, constraints: { :id => /.+/ }
-  resources :prefixes, constraints: { :id => /.+/ }
-  resources :allocators
-  resources :members, controller: 'allocators'
-  resources :datacenters, controller: 'datacentres'
+  resources :index, path: '/lupo', only: [:index]
+  resources :datacentres, path: '/lupo/datacentres',  constraints: { :id => /.+/ }
+  resources :datasets,  path: '/lupo/datasets', constraints: { :id => /.+/ }
+  resources :prefixes,  path: '/lupo/prefixes', constraints: { :id => /.+/ }
+  resources :allocators, path: '/lupo/allocators'
+  resources :members, path: '/lupo/members',  controller: 'allocators'
+  resources :datacenters,  path: '/lupo/datacenters', controller: 'datacentres'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
