983273b24c022477660074ea506d466e3e32f48b
Martin Fenner <mfenner@datacite.org>
2017-08-12 10:52:50 +0200

    use alias_attribute in model and datacenter

--- app/models/datacenter.rb
+++ app/models/datacenter.rb
@@ -1,28 +1,27 @@
 class Datacenter < ApplicationRecord
+  # define table and attribute names
+  # we rename name -> title, so that we can do symbol -> name
+  # name is used as unique identifier for most of our records,
+  # mapped to id in serializer
   self.table_name = "datacentre"
-  # alias_attribute :created_at, :created
-  # alias_attribute :updated_at, :updated
-  attribute :member_id
+  alias_attribute :title, :name
+  alias_attribute :name, :symbol
   alias_attribute :member_id, :allocator
-  attribute :datacenter_id
-  alias_attribute :datacenter_id, :symbol
-  # attribute :title
-  # alias_attribute :title, :symbol
-  validates_presence_of :name, :member_id
-  validates_presence_of :datacenter_id
-  validates_uniqueness_of :datacenter_id, message: "This datacenter_id has already been taken"
+  alias_attribute :created_at, :created
+  alias_attribute :updated_at, :updated
+
+  validates_presence_of :name, :title, :member_id, :contact_email, :contact_name, :doi_quota_allowed, :doi_quota_used
+  validates_uniqueness_of :name, message: "This name has already been taken"
   validates_format_of :contact_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_numericality_of :doi_quota_allowed, :doi_quota_used
   validates_numericality_of :version, if: :version?
-  validates_inclusion_of :role_name, :in => %w( ROLE_DATACENTRE ), :message => "Role {{value}} is not included in the list"
-
+  validates_inclusion_of :role_name, :in => %w( ROLE_DATACENTRE ), :message => "Role %s is not included in the list"
 
   has_and_belongs_to_many :prefixes, class_name: 'Prefix', join_table: "datacentre_prefixes", foreign_key: :prefixes, association_foreign_key: :datacentre
   belongs_to :member, class_name: 'Member', foreign_key: :allocator
   has_many :datasets
 
-  after_create  :add_test_prefix
-
+  # after_create  :add_test_prefix
 
   #  * Increase used quota counter for a datacentre.
   #  *
@@ -88,7 +87,5 @@ class Datacenter < ApplicationRecord
 
   def add_test_prefix
 
-
   end
-
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,27 +1,33 @@
+require "countries"
+
 class Member < ApplicationRecord
-  require "countries"
+  # define table and attribute names
+  # we rename name -> title, so that we can do symbol -> name
+  # name is used as unique identifier for most of our records,
+  # mapped to id in serializer
   self.table_name = "allocator"
-  # alias_attribute :created_at, :created
-  # alias_attribute :updated_at, :updated
-  attribute :member_id
-  alias_attribute :member_id, :symbol
-  has_many :datacenters
-  validates_presence_of :name
-  validates_uniqueness_of :member_id, message: "This member_id has already been taken"
+  alias_attribute :title, :name
+  alias_attribute :name, :symbol
+  alias_attribute :created_at, :created
+  alias_attribute :updated_at, :updated
+
+  validates_presence_of :name, :title, :contact_email, :contact_name, :doi_quota_allowed, :doi_quota_used, :country_code
+  validates_uniqueness_of :name, message: "This name has already been taken"
   validates_format_of :contact_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: "contact_email should be an email"
   validates_format_of :website, :with => /https?:\/\/[\S]+/ , if: :website?, message: "Website should be an url"
-  validates_format_of :logo, :with => /https?:\/\/[\S]+/ , if: :logo?, message: "Logo should be an url"
+  validates_numericality_of :doi_quota_allowed, :doi_quota_used
   validates_numericality_of :version, if: :version?
-  validates_numericality_of :doi_quota_allowed, :version
-  validates_inclusion_of :role_name, :in => %w( ROLE_ALLOCATOR ROLE_ADMIN ROLE_DEV ), :message => "Role {{value}} is not included in the list", if: :role_name?
+  validates_inclusion_of :role_name, :in => %w( ROLE_ALLOCATOR ROLE_ADMIN ROLE_DEV ), :message => "Role %s is not included in the list", if: :role_name?
   validates_inclusion_of :country_code, :in => ISO3166::Country.all.map(&:alpha2), :message => "must be a 2 character country representation (ISO 3166-1)", if: :country_code?
 
+  has_many :datacenters
   has_and_belongs_to_many :prefixes, class_name: 'Prefix', join_table: "allocator_prefixes", foreign_key: :prefixes, association_foreign_key: :allocator
 
-  after_create  :add_test_prefix
+  # after_create  :add_test_prefix
 
   def member_type
-    return "allocating"  if doi_quota_allowed >= 0
+    return "allocating" if doi_quota_allowed > 0
+
     "non_allocating"
   end
 end
