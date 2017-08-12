9cbab5ee2375e0285d0710e94bb8ecc15d2f0a96
Martin Fenner <mfenner@datacite.org>
2017-08-12 09:51:11 +0200

    only keep default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :heartbeat, only: [:index]
   resources :index, path: '/', only: [:index]
-  resources :datacenters, constraints: { :id => /.+/ }
+  resources :datacenters, path: "/data-centers", constraints: { :id => /.+/ }
   resources :datasets, constraints: { :id => /.+/ }
   resources :prefixes, constraints: { :id => /.+/ }
   resources :members, constraints: { :id => /.+/ }
-  # resources :members, path: '/members', controller: 'allocators'
-  resources :data_centers, path: "/data-centers", controller: 'datacenters'
 end
