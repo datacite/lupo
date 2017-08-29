12672bec3778acedde59fb6ee617931609360f13
Martin Fenner <mfenner@datacite.org>
2017-08-29 23:54:01 +0200

    show domains as array

--- app/serializers/datacenter_serializer.rb
+++ app/serializers/datacenter_serializer.rb
@@ -2,41 +2,17 @@ class DatacenterSerializer < ActiveModel::Serializer
   # type 'data-centers'
   cache key: 'data-center'
 
-  attributes :name, :prefixes, :domains, :member_id, :year, :created, :updated
+  attributes :name, :domains, :member_id, :year, :created, :updated
+
+  has_many :datasets
+  has_many :prefixes
+  belongs_to :member
 
   def id
     object.uid.downcase
   end
 
-  has_many :datasets
-  has_many :prefixes
-  belongs_to :member, serializer: MemberSerializer
-
-  # def name
-  #   object.name
-  # end
-  #
-  # def prefixes
-  #   object.prefixes
-  # end
-  #
-  # def domains
-  #   object.domains #object.domains.to_s.split(/\s*,\s*/).presence
-  # end
-  #
-  # def member_id
-  #  object.member_id
-  # end
-  #
-  # def year
-  #  object.year
-  # end
-  #
-  # def created
-  #   object.created
-  # end
-  #
-  # def updated
-  #   object.updated
-  # end
+  def domains
+    object.domains.to_s.split(/\s*,\s*/).presence
+  end
 end
