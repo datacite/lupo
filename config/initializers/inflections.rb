d8435fa7c182a6379d070b62b6be238bfed75509
Martin Fenner <mfenner@datacite.org>
2017-08-30 07:41:15 +0200

    consistently use medium as singular

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,5 +2,6 @@
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.irregular 'medium', 'media'
   inflect.uncountable %w( status heartbeat )
 end
