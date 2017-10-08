8d369df52564856986f972ccb45b5f7254d5cf82
Martin Fenner <mfenner@datacite.org>
2017-10-09 00:32:02 +0200

    facet clients by year

--- app/controllers/concerns/facetable.rb
+++ app/controllers/concerns/facetable.rb
@@ -2,10 +2,6 @@ module Facetable
   extend ActiveSupport::Concern
 
   include do
-    def serialize(errors)
-
-    end
-
     def client_year_facet(params, collection)
       [{ id: params[:year],
          title: params[:year],
