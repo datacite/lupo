664c41b2f5b938087f88c5ebd7695de377724ce9
Martin Fenner <mfenner@datacite.org>
2017-11-12 16:06:37 +0100

    added ability specs

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,202 @@
+require 'rails_helper'
+require "cancan/matchers"
+
+describe User, type: :model do
+  let(:token) { User.generate_token }
+  let(:user) { User.new(token) }
+  let(:provider) { create(:provider) }
+  let(:client) { create(:client, provider_id: provider.symbol) }
+  let(:doi) { create(:doi, client_id: client.symbol) }
+  let(:prefix) { create(:prefix) }
+  let(:client_prefix) { create(:client_prefix, client: client) }
+  let(:provider_prefix) { create(:provider_prefix, provider: provider) }
+
+  describe 'User attributes', :order => :defined do
+    it "is valid with valid attributes" do
+      expect(user.name).to eq("Josiah Carberry")
+    end
+  end
+
+  describe "abilities" do
+    subject { Ability.new(user) }
+
+    context "when is a user" do
+      let(:token){ User.generate_token(role_id: "user") }
+
+      it{ is_expected.to be_able_to(:read, user) }
+
+      it{ is_expected.not_to be_able_to(:read, provider) }
+      it{ is_expected.not_to be_able_to(:create, provider) }
+      it{ is_expected.not_to be_able_to(:update, provider) }
+      it{ is_expected.not_to be_able_to(:destroy, provider) }
+
+      it{ is_expected.not_to be_able_to(:read, client) }
+      it{ is_expected.not_to be_able_to(:create, client) }
+      it{ is_expected.not_to be_able_to(:update, client) }
+      it{ is_expected.not_to be_able_to(:destroy, client) }
+
+      it{ is_expected.not_to be_able_to(:read, prefix) }
+      it{ is_expected.not_to be_able_to(:create, prefix) }
+      it{ is_expected.not_to be_able_to(:update, prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, prefix) }
+    end
+
+    context "when is a client admin" do
+      let(:token){ User.generate_token(role_id: "client_admin", provider_id: provider.symbol.downcase, client_id: client.symbol.downcase) }
+
+      it{ is_expected.to be_able_to(:read, user) }
+
+      it{ is_expected.not_to be_able_to(:read, provider) }
+      it{ is_expected.not_to be_able_to(:create, provider) }
+      it{ is_expected.not_to be_able_to(:update, provider) }
+      it{ is_expected.not_to be_able_to(:destroy, provider) }
+
+      it{ is_expected.to be_able_to(:read, client) }
+      it{ is_expected.not_to be_able_to(:create, client) }
+      it{ is_expected.to be_able_to(:update, client) }
+      it{ is_expected.not_to be_able_to(:destroy, client) }
+
+      it{ is_expected.not_to be_able_to(:read, prefix) }
+      it{ is_expected.not_to be_able_to(:create, prefix) }
+      it{ is_expected.not_to be_able_to(:update, prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, prefix) }
+
+      it{ is_expected.to be_able_to(:read, client_prefix) }
+      it{ is_expected.not_to be_able_to(:create, client_prefix) }
+      it{ is_expected.not_to be_able_to(:update, client_prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, client_prefix) }
+    end
+
+    context "when is a client user" do
+      let(:token){ User.generate_token(role_id: "client_user", provider_id: provider.symbol.downcase, client_id: client.symbol.downcase) }
+
+      it{ is_expected.to be_able_to(:read, user) }
+
+      it{ is_expected.not_to be_able_to(:read, provider) }
+      it{ is_expected.not_to be_able_to(:create, provider) }
+      it{ is_expected.not_to be_able_to(:update, provider) }
+      it{ is_expected.not_to be_able_to(:destroy, provider) }
+
+      it{ is_expected.to be_able_to(:read, client) }
+      it{ is_expected.not_to be_able_to(:create, client) }
+      it{ is_expected.not_to be_able_to(:update, client) }
+      it{ is_expected.not_to be_able_to(:destroy, client) }
+
+      it{ is_expected.not_to be_able_to(:read, prefix) }
+      it{ is_expected.not_to be_able_to(:create, prefix) }
+      it{ is_expected.not_to be_able_to(:update, prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, prefix) }
+
+      it{ is_expected.to be_able_to(:read, client_prefix) }
+      it{ is_expected.not_to be_able_to(:create, client_prefix) }
+      it{ is_expected.not_to be_able_to(:update, client_prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, client_prefix) }
+    end
+
+    context "when is a provider admin" do
+      let(:token){ User.generate_token(role_id: "provider_admin", provider_id: provider.symbol.downcase) }
+
+      it{ is_expected.to be_able_to(:read, user) }
+
+      it{ is_expected.to be_able_to(:read, provider) }
+      it{ is_expected.not_to be_able_to(:create, provider) }
+      it{ is_expected.to be_able_to(:update, provider) }
+      it{ is_expected.not_to be_able_to(:destroy, provider) }
+
+      it{ is_expected.to be_able_to(:read, client) }
+      it{ is_expected.to be_able_to(:create, client) }
+      it{ is_expected.to be_able_to(:update, client) }
+      it{ is_expected.to be_able_to(:destroy, client) }
+
+      it{ is_expected.not_to be_able_to(:read, prefix) }
+      it{ is_expected.not_to be_able_to(:create, prefix) }
+      it{ is_expected.not_to be_able_to(:update, prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, prefix) }
+
+      it{ is_expected.to be_able_to(:read, provider_prefix) }
+      it{ is_expected.to be_able_to(:create, provider_prefix) }
+      it{ is_expected.to be_able_to(:update, provider_prefix) }
+      it{ is_expected.to be_able_to(:destroy, provider_prefix) }
+    end
+
+    context "when is a provider user" do
+      let(:token){ User.generate_token(role_id: "provider_user", provider_id: provider.symbol.downcase) }
+
+      it{ is_expected.to be_able_to(:read, user) }
+
+      it{ is_expected.to be_able_to(:read, provider) }
+      it{ is_expected.not_to be_able_to(:create, provider) }
+      it{ is_expected.not_to be_able_to(:update, provider) }
+      it{ is_expected.not_to be_able_to(:destroy, provider) }
+
+      it{ is_expected.to be_able_to(:read, client) }
+      it{ is_expected.not_to be_able_to(:create, client) }
+      it{ is_expected.not_to be_able_to(:update, client) }
+      it{ is_expected.not_to be_able_to(:destroy, client) }
+
+      it{ is_expected.not_to be_able_to(:read, prefix) }
+      it{ is_expected.not_to be_able_to(:create, prefix) }
+      it{ is_expected.not_to be_able_to(:update, prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, prefix) }
+
+      it{ is_expected.to be_able_to(:read, provider_prefix) }
+      it{ is_expected.not_to be_able_to(:create, provider_prefix) }
+      it{ is_expected.not_to be_able_to(:update, provider_prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, provider_prefix) }
+    end
+
+    context "when is a staff admin" do
+      it{ is_expected.to be_able_to(:read, user) }
+
+      it{ is_expected.to be_able_to(:read, provider) }
+      it{ is_expected.to be_able_to(:create, provider) }
+      it{ is_expected.to be_able_to(:update, provider) }
+      it{ is_expected.to be_able_to(:destroy, provider) }
+
+      it{ is_expected.to be_able_to(:read, client) }
+      it{ is_expected.to be_able_to(:create, client) }
+      it{ is_expected.to be_able_to(:update, client) }
+      it{ is_expected.to be_able_to(:destroy, client) }
+
+      it{ is_expected.to be_able_to(:read, doi) }
+    end
+
+    context "when is a staff user" do
+      let(:token){ User.generate_token(role_id: "staff_user") }
+
+      it{ is_expected.to be_able_to(:read, user) }
+
+      it{ is_expected.to be_able_to(:read, provider) }
+      it{ is_expected.not_to be_able_to(:create, provider) }
+      it{ is_expected.not_to be_able_to(:update, provider) }
+      it{ is_expected.not_to be_able_to(:destroy, provider) }
+
+      it{ is_expected.to be_able_to(:read, client) }
+      it{ is_expected.not_to be_able_to(:create, client) }
+      it{ is_expected.not_to be_able_to(:update, client) }
+      it{ is_expected.not_to be_able_to(:destroy, client) }
+
+      it{ is_expected.to be_able_to(:read, doi) }
+    end
+
+    context "when is anonymous" do
+      let(:token) { nil }
+
+      it{ is_expected.not_to be_able_to(:read, provider) }
+      it{ is_expected.not_to be_able_to(:update, provider) }
+      it{ is_expected.not_to be_able_to(:destroy, provider) }
+
+      it{ is_expected.not_to be_able_to(:read, client) }
+      it{ is_expected.not_to be_able_to(:create, client) }
+      it{ is_expected.not_to be_able_to(:update, client) }
+      it{ is_expected.not_to be_able_to(:destroy, client) }
+
+      it{ is_expected.not_to be_able_to(:read, doi) }
+
+      it{ is_expected.not_to be_able_to(:read, prefix) }
+      it{ is_expected.not_to be_able_to(:create, prefix) }
+      it{ is_expected.not_to be_able_to(:update, prefix) }
+      it{ is_expected.not_to be_able_to(:destroy, prefix) }
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,70 +1,12 @@
-# require 'rails_helper'
-# require "cancan/matchers"
-#
-# describe User, type: :model do
-#   let(:user) { User.new(ENV['JWT_TOKEN']) }
-#
-#   describe 'User attributes', :order => :defined do
-#     it "is valid with valid attributes" do
-#       expect(user.name).to eq("Josiah Carberry{n}")
-#     end
-#   end
-#
-#   describe "abilities" do
-#     subject { Ability.new(user) }
-#
-#     context "when is a data centre admin" do
-#       let(:role){ "client_admin" }
-#
-#       it{ is_expected.not_to be_able_to(:create, Provider.new) }
-#       it{ is_expected.to be_able_to(:read, FactoryBot.create(:client)) }
-#       it{ is_expected.not_to be_able_to(:update, Provider.new) }
-#       it{ is_expected.not_to be_able_to(:destroy, Provider.new) }
-#     end
-#   #
-#   #   context "when is a data centre user" do
-#   #     let(:role){ "client_user" }
-#   #
-#   #     it{ is_expected.not_to be_able_to(:create, Allocator.new) }
-#   #     it{ is_expected.to be_able_to(:read, FactoryGirl.create(:client)) }
-#   #     it{ is_expected.not_to be_able_to(:update, Allocator.new) }
-#   #     it{ is_expected.not_to be_able_to(:destroy, Allocator.new) }
-#   #   end
-#   #
-#   #   context "when is a anonymous" do
-#   #     let(:role){ "anonymous" }
-#   #
-#   #     it{ is_expected.not_to be_able_to(:read, Allocator.new) }
-#   #     it{ is_expected.to be_able_to(:read, Dataset.new) }
-#   #     it{ is_expected.not_to be_able_to(:update, Allocator.new) }
-#   #     it{ is_expected.not_to be_able_to(:destroy, Allocator.new) }
-#   #   end
-#   #
-#   #   context "when is a provider admin" do
-#   #     let(:role){ "provider_admin" }
-#   #
-#   #     it{ is_expected.not_to be_able_to(:create, Allocator.new) }
-#   #     it{ is_expected.to be_able_to(:read, FactoryGirl.create(:client)) }
-#   #     it{ is_expected.not_to be_able_to(:update, Allocator.new) }
-#   #     it{ is_expected.not_to be_able_to(:destroy, Allocator.new) }
-#   #   end
-#   #
-#   #   context "when is a provider user" do
-#   #     let(:role) { "provider_user" }
-#   #
-#   #     it{ is_expected.not_to be_able_to(:create, Allocator.new) }
-#   #     it{ is_expected.to be_able_to(:read, FactoryGirl.create(:client)) }
-#   #     it{ is_expected.not_to be_able_to(:update, Allocator.new) }
-#   #     it{ is_expected.not_to be_able_to(:destroy, Allocator.new) }
-#   #   end
-#   #
-#   #   context "when is a staff admin" do
-#   #     let(:role) { "staff_admin" }
-#   #
-#   #     it{ is_expected.not_to be_able_to(:create, Allocator.new) }
-#   #     it{ is_expected.to be_able_to(:read, FactoryGirl.create(:client)) }
-#   #     it{ is_expected.not_to be_able_to(:update, Allocator.new) }
-#   #     it{ is_expected.not_to be_able_to(:destroy, Allocator.new) }
-#   #   end
-#   end
-# end
+require 'rails_helper'
+
+describe User, type: :model do
+  let(:token) { User.generate_token }
+  let(:user) { User.new(token) }
+
+  describe 'User attributes', :order => :defined do
+    it "ha name" do
+      expect(user.name).to eq("Josiah Carberry")
+    end
+  end
+end
