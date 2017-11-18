55bd613ec04c4c1ca6d8df2d1bb74b874d874f62
Martin Fenner <mfenner@datacite.org>
2017-11-18 08:13:13 +0100

    jsonapi mime type is already registered by active_model_serializer

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,6 +2,3 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-Mime::Type.unregister :json
-Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.api+json )
-Mime::Type.register "image/svg+xml", :svg
