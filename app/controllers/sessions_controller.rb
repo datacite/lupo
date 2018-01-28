9e7b7b9af8234d1c068dfcf4e19ec348348ab594
Martin Fenner <mfenner@datacite.org>
2018-01-28 08:40:56 +0100

    simplify sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,33 +3,28 @@ class SessionsController < ApplicationController
 
   def create
     if safe_params[:grant_type] != "password"
-      message = "Wrong grant type."
-      status = 400
-      Rails.logger.warn message
-      render json: { errors: [{ status: status.to_s, title: message }] }.to_json, status: status
+      error_response("Wrong grant type.")
     elsif safe_params[:username].blank? || safe_params[:password].blank?
-      message = "Missing username or password."
-      status = 400
-      Rails.logger.warn message
-      render json: { errors: [{ status: status.to_s, title: message }] }.to_json, status: status
+      error_response("Missing username or password.")
     else
       credentials = User.encode_auth_param(username: safe_params[:username], password: safe_params[:password])
-      Rails.logger.warn credentials
       user = User.new(credentials, type: "basic")
 
       if user.role_id == "anonymous"
-        message = "Wrong username or password."
-        status = 400
-        Rails.logger.warn message
-        render json: { errors: [{ status: status.to_s, title: message }] }.to_json, status: status
+        error_response("Wrong username or password.")
       else
-        @current_user = user
         render json: { "access_token" => user.jwt, "expires_in" => 3600 * 24 * 30 }.to_json, status: 200
       end
     end
   end
 
   private
+
+  def error_response(message)
+    status = 400
+    Rails.logger.warn message
+    render json: { errors: [{ status: status.to_s, title: message }] }.to_json, status: status
+  end
 
   def safe_params
     params.permit(:grant_type, :username, :password, :client_id, :client_secret, :format)
