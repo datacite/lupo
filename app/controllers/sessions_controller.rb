ac74c325aee6245102c7014896ad942b64b4d456
Martin Fenner <mfenner@datacite.org>
2018-01-28 08:45:28 +0100

    clean up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,15 @@
 class SessionsController < ApplicationController
-  before_action :authenticate_user!, only: [:destroy]
-
   def create
-    if safe_params[:grant_type] != "password"
-      error_response("Wrong grant type.")
-    elsif safe_params[:username].blank? || safe_params[:password].blank?
-      error_response("Missing username or password.")
-    else
-      credentials = User.encode_auth_param(username: safe_params[:username], password: safe_params[:password])
-      user = User.new(credentials, type: "basic")
+    error_response("Wrong grant type.") && return if safe_params[:grant_type] != "password"
+    error_response("Missing username or password.") && return if
+      safe_params[:username].blank? || safe_params[:password].blank?
 
-      if user.role_id == "anonymous"
-        error_response("Wrong username or password.")
-      else
-        render json: { "access_token" => user.jwt, "expires_in" => 3600 * 24 * 30 }.to_json, status: 200
-      end
-    end
+    credentials = User.encode_auth_param(username: safe_params[:username], password: safe_params[:password])
+    user = User.new(credentials, type: "basic")
+
+    error_response("Wrong username or password.") && return if user.role_id == "anonymous"
+
+    render json: { "access_token" => user.jwt, "expires_in" => 3600 * 24 * 30 }.to_json, status: 200
   end
 
   private
