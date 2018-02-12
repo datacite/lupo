51a453e8adb16b4bbf15c398d6848dcf0e629658
Kristian Garza <kj.garza@gmail.com>
2018-02-12 16:55:32 +0100

    align indent

--- app/models/concerns/indexable.rb
+++ app/models/concerns/indexable.rb
@@ -2,10 +2,7 @@ module Indexable
     extend ActiveSupport::Concern
   
     included do
-
       after_save    {ElasticsearchJob.perform_later( self.to_jsonapi, "index")}
-
       after_destroy {ElasticsearchJob.perform_later( self.to_jsonapi, "delete")}
-
     end
-  end
+end
