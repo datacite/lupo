28bff7daa6fc742fbf8ba6c3e15c1308953df552
Martin Fenner <mfenner@datacite.org>
2018-02-24 13:52:42 +0100

    prefix shoryuken queues

--- config/initializers/_shoryuken.rb
+++ config/initializers/_shoryuken.rb
@@ -2,3 +2,5 @@ Shoryuken.configure_server do |config|
   Rails.logger = Shoryuken::Logging.logger
   Rails.logger.level = Logger.const_get(ENV["LOG_LEVEL"].upcase)
 end
+
+Shoryuken.active_job_queue_name_prefixing = true
