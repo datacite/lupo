dd557d1bc3d9e37c96da9f5484c2ac7188962197
Martin Fenner <mfenner@datacite.org>
2018-02-25 08:33:38 +0100

    use different message header for elastic delete message

--- app/models/concerns/indexable.rb
+++ app/models/concerns/indexable.rb
@@ -5,22 +5,30 @@ module Indexable
 
   included do
     unless Rails.env.test?
-      before_destroy { send_message(data: self.to_jsonapi, action: "destroy") }
-      after_create { send_message(data: self.to_jsonapi, action: "create") }
-      after_update { send_message(data: self.to_jsonapi, action: "update") }
+      before_destroy { send_delete_message(self.to_jsonapi) }
+      after_save { send_import_message(self.to_jsonapi) }
     end
 
+    def send_delete_message(data)
+      send_message(data, shoryuken_class: "ElasticDeleteWorker")
+    end
+
+    def send_import_message(data)
+      send_message(data, shoryuken_class: "ElasticImportWorker")
+    end
+    
     # shoryuken_class is needed for the consumer to process the message
     # we use the AWS SQS client directly as there is no consumer in this app
-    def send_message(body)
+    def send_message(body, options={})
       sqs = Aws::SQS::Client.new
       queue_url = sqs.get_queue_url(queue_name: "#{Rails.env}_elastic").queue_url
+      options[:shoryuken_class] ||= "ElasticImportWorker"
 
       options = {
         queue_url: queue_url,
         message_attributes: {
           'shoryuken_class' => {
-            string_value: "ElasticWorker",
+            string_value: options[:shoryuken_class],
             data_type: 'String'
           },
         },
--- spec/concerns/indexable_spec.rb
+++ spec/concerns/indexable_spec.rb
@@ -3,9 +3,18 @@ require 'rails_helper'
 describe "Indexable", vcr: true do
   subject  { create(:client) }
 
-  it 'send message' do
-    message_body = { data: subject.to_jsonapi, action: "create" }
-    response = subject.send_message(message_body)
+  it 'send_message' do
+    response = subject.send_message(subject.to_jsonapi)
+    expect(response.message_id).to be_present
+  end
+
+  it 'send_import_message' do
+    response = subject.send_import_message(subject.to_jsonapi)
+    expect(response.message_id).to be_present
+  end
+
+  it 'send_delete_message' do
+    response = subject.send_delete_message(subject.to_jsonapi)
     expect(response.message_id).to be_present
   end
 end
