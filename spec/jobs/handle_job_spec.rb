7189fe37f4354ea848e9e7d0d062522002b7058d
Martin Fenner <mfenner@datacite.org>
2018-02-28 19:38:11 +0100

    use correct job name

--- spec/jobs/handle_job_spec.rb
+++ spec/jobs/handle_job_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe HandleJob, type: :job do
   let(:doi) { create(:doi) }
-  subject(:job) { UrlJob.perform_later(doi) }
+  subject(:job) { HandleJob.perform_later(doi) }
 
   it 'queues the job' do
-    expect { job }.to have_enqueued_job(UrlJob)
+    expect { job }.to have_enqueued_job(HandleJob)
       .on_queue("test_lupo")
   end
 end
