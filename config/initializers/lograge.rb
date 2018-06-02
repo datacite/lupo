d4a41e26724f95c21a2a09ffa22d0388a145c77e
Martin Fenner <martin.fenner@datacite.org>
2018-06-02 13:32:46 +0200

    include severity, host and params #67

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,9 +1,16 @@
 Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Logstash.new
-  config.lograge.logger = ActiveSupport::Logger.new(STDOUT)
+  config.lograge.logger = LogStashLogger.new(type: :stdout)
 
   config.lograge.ignore_actions = ['HeartbeatController#index', 'IndexController#index']
   config.lograge.base_controller_class = 'ActionController::API'
   config.log_level = ENV['LOG_LEVEL'].to_sym
+
+  config.lograge.custom_options = lambda do |event|
+    exceptions = %w(controller action format id)
+    {
+      params: event.payload[:params].except(*exceptions)
+    }
+  end
 end
