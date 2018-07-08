b2538bc6ddd96369df6b6536798ce620aaf9cc0a
Martin Fenner <martin.fenner@datacite.org>
2018-07-08 12:08:31 +0200

    better index creation in tests

--- spec/support/elasticsearch_helper.rb
+++ spec/support/elasticsearch_helper.rb
@@ -4,7 +4,11 @@ SEARCHABLE_MODELS = [Client, Provider, Doi, Prefix]
 RSpec.configure do |config|
   config.around :each, elasticsearch: true do |example|
     SEARCHABLE_MODELS.each do |model|
-      model.__elasticsearch__.create_index! force: true rescue nil
+      if Elasticsearch::Model.client.indices.exists? index: model.index_name
+        model.__elasticsearch__.create_index! force: true
+      else
+        model.__elasticsearch__.create_index!
+      end
     end
 
     example.run
