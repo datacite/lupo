38266d0945a564b3bc184bded49a0da917f5ee80
Martin Fenner <martin.fenner@datacite.org>
2018-08-05 11:44:57 +0200

    use phrase_prefix query type. #95

--- app/models/concerns/indexable.rb
+++ app/models/concerns/indexable.rb
@@ -103,7 +103,7 @@ module Indexable
       options[:sort] ||= { "_doc" => { order: 'asc' }}
 
       must = []
-      must << { multi_match: { query: query, fields: query_fields }} if query.present?
+      must << { multi_match: { query: query, fields: query_fields, type: "phrase_prefix", max_expansions: 50 }} if query.present?
       must << { term: { aasm_state: options[:state] }} if options[:state].present?
       must << { term: { resource_type_general: options[:resource_type_id] }} if options[:resource_type_id].present?
       must << { terms: { provider_id: options[:provider_id].split(",") }} if options[:provider_id].present?
