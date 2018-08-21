0a9d288609d62f1648e31d97e5ba78a24dd390c4
Martin Fenner <martin.fenner@datacite.org>
2018-08-21 12:51:55 +0200

    use correct type

--- app/serializers/client_prefix_serializer.rb
+++ app/serializers/client_prefix_serializer.rb
@@ -9,6 +9,6 @@ class ClientPrefixSerializer
 
   belongs_to :client, record_type: :clients
   belongs_to :provider, record_type: :providers
-  belongs_to :provider_prefix, record_type: :prefix_providers
+  belongs_to :provider_prefix, record_type: :provider_prefixes
   belongs_to :prefix, record_type: :prefixes
 end
