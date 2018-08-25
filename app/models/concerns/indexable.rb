d7be61ecc3435ff2eab7ab8f88517062461e6279
Martin Fenner <martin.fenner@datacite.org>
2018-08-25 16:29:09 +0200

    logging for indexing delay. datacite/datacite#471

--- app/models/concerns/indexable.rb
+++ app/models/concerns/indexable.rb
@@ -8,7 +8,7 @@ module Indexable
       # use index_document instead of update_document to also update virtual attributes
       IndexJob.perform_later(self)
       if self.class.name == "Doi" && !Rails.env.test?
-        index_interval = Time.zone.now - updated_at
+        index_interval = (Time.zone.now - updated_at).to_i
         Rails.logger.info "[Elasticsearch] Indexing of DOI #{doi} finished #{index_interval.to_s} seconds after DOI update."
         send_import_message(self.to_jsonapi)
       end
