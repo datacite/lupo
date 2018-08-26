91d14169a603e0590752b77fa4910136e1b38fa9
Martin Fenner <martin.fenner@datacite.org>
2018-08-26 15:06:24 +0200

    support ftp, s3 and gs protocol for media. #107

--- app/models/media.rb
+++ app/models/media.rb
@@ -6,16 +6,15 @@ class Media < ActiveRecord::Base
 
   alias_attribute :created_at, :created
   alias_attribute :updated_at, :updated
-  validates_presence_of  :url, :media_type
-  validates_format_of :url, :with => /https?:\/\/[\S]+/, if: :url?
+  validates_presence_of  :url
+  validates_format_of :url, :with => /\A(ftp|http|https|gs|s3):\/\/[\S]+/, if: :url?
   validates_format_of :media_type, :with => /[\S]+\/[\S]+/, if: :media_type?
   validates_associated :doi
 
   belongs_to :doi, foreign_key: :dataset
 
-  before_validation :set_media_version
   before_create { self.created = Time.zone.now.utc.iso8601 }
-  before_save { self.updated = Time.zone.now.utc.iso8601 }
+  before_save :set_defaults
 
   def uid
     Base32::URL.encode(id, split: 4, length: 16)
@@ -32,8 +31,10 @@ class Media < ActiveRecord::Base
     write_attribute(:dataset, r.id)
   end
 
-  def set_media_version
+  def set_defaults
     current_media = Media.where(dataset: dataset).order('media.created DESC').first
     self.version = current_media.present? ? current_media.version + 1 : 0
+    self.media_type = "text/plain " if media_type.blank?
+    self.updated = Time.zone.now.utc.iso8601
   end
 end
