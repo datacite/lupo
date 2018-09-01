6798c3c1552c231307e056a019b8e872717f4579
Martin Fenner <martin.fenner@datacite.org>
2018-09-01 17:48:10 +0200

    pritoritize alternate_identifier in doi queries

--- app/models/doi.rb
+++ app/models/doi.rb
@@ -220,7 +220,7 @@ class Doi < ActiveRecord::Base
   end
 
   def self.query_fields
-    ['doi^10', 'title_normalized^10', 'author_names^10', 'author_normalized.name^10', 'author_normalized.id^10', 'publisher^10', 'description_normalized^10', 'resource_type_id^10', 'resource_type_subtype^10', 'alternate_identifier', '_all']
+    ['doi^10', 'title_normalized^10', 'author_names^10', 'author_normalized.name^10', 'author_normalized.id^10', 'publisher^10', 'description_normalized^10', 'resource_type_id^10', 'resource_type_subtype^10', 'alternate_identifier.name^10', '_all']
   end
 
   def self.find_by_id(id, options={})
