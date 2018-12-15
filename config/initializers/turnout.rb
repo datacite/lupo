4c65d1b3b0f7198a48b493c18dd9babe5b8e1e59
Martin Fenner <martin.fenner@datacite.org>
2018-12-15 10:43:19 +0100

    allow health checks. #158

--- config/initializers/turnout.rb
+++ config/initializers/turnout.rb
@@ -1,4 +1,5 @@
 Turnout.configure do |config|
   config.default_maintenance_page = Turnout::MaintenancePage::JSON
+  config.default_allowed_paths = ['^/heartbeat']
   config.default_reason = "The site is temporarily down for maintenance. Please check https://status.datacite.org for more information."
 end
