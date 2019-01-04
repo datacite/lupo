0d1dfdcce8189299460ad76c0a387356042b5803
Martin Fenner <martin.fenner@datacite.org>
2019-01-04 11:49:31 +0100

    use excon for requests to elasticsearch. #178

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -3,6 +3,7 @@ class ClientSerializer
   set_key_transform :camel_lower
   set_type :clients
   set_id :uid
+  cache_options enabled: true, cache_length: 24.hours
   
   attributes :name, :symbol, :year, :contact_name, :contact_email, :description, :domains, :url, :created, :updated
 
--- config/initializers/_version.rb
+++ config/initializers/_version.rb
@@ -1,5 +1,5 @@
 module Lupo
   class Application
-    VERSION = "2.3"
+    VERSION = "2.3.1"
   end
 end
