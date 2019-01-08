c5fdb2bed524976d9f38c0987de1f070c82e0e81
Kristian Garza <kj.garza@gmail.com>
2019-01-08 13:38:51 +0100

    new facets

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -129,14 +129,16 @@ class ClientsController < ApplicationController
 
   def totals
     page ={size: 25, number: 1}
-    ttl = Client.all.map do |client|
+    page_prov = { size: 2000, number: 1}
+
+    ttl = Client.query("", page: page_prov).map do |client|    
       response = Doi.query("", client_id: client.symbol.downcase, page: page)
       total = response.results.total
       states = total > 0 ? facet_by_key(response.response.aggregations.states.buckets) : nil
       temporal ={}
-      temporal[:last_thirty] = total > 0 ? facet_by_key(response.response.aggregations.last_thirty_days.buckets) : nil
-      temporal[:last_year] = total > 0 ? facet_by_key(response.response.aggregations.last_year.buckets) : nil
-      temporal[:last_two_year] = total > 0 ? facet_by_key(response.response.aggregations.last_two_year.buckets) : nil
+      temporal[:this_month] = total > 0 ? facet_by_date(response.response.aggregations.this_month.buckets) : nil
+      temporal[:this_year] = total > 0 ? facet_anual(response.response.aggregations.this_year.buckets) : nil
+      temporal[:last_year] = total > 0 ? facet_anual(response.response.aggregations.last_year.buckets) : nil
       id = client.symbol
       {id: id, title: id, count: total, states: states, temporal: temporal}
     end
--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -121,18 +121,18 @@ class ProvidersController < ApplicationController
   end
 
   def totals
-    page ={size: 25, number: 1}
-    logger = Logger.new(STDOUT)
+    page = { size: 25, number: 1}
+    page_prov = { size: 500, number: 1}
 
-    ttl = Provider.all.map do |provider|
+    ttl = Provider.query("", page: page_prov).map do |provider|      
       response = Doi.query("", provider_id: provider.symbol.downcase, page: page)
-      logger.info response.results.total
       total = response.results.total
       states = total > 0 ? facet_by_key(response.response.aggregations.states.buckets) : nil
       temporal ={}
-      temporal[:last_thirty] = total > 0 ? facet_by_key(response.response.aggregations.last_thirty_days.buckets) : nil
-      temporal[:last_year] = total > 0 ? facet_by_key(response.response.aggregations.last_year.buckets) : nil
-      temporal[:last_two_year] = total > 0 ? facet_by_key(response.response.aggregations.last_two_year.buckets) : nil
+      temporal[:this_month] = total > 0 ? facet_by_date(response.response.aggregations.this_month.buckets) : nil
+      temporal[:this_year] = total > 0 ? facet_anual(response.response.aggregations.this_year.buckets) : nil
+      temporal[:last_year] = total > 0 ? facet_anual(response.response.aggregations.last_year.buckets) : nil
+      # temporal[:providers] = total > 0 ? facet_by_key(response.response.aggregations.provider_x.buckets) : nil
       id = provider.symbol
       {id: id, title: id, count: total, states: states, temporal: temporal}
     end
