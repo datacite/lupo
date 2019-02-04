0b331d690324aecffb11dee287fcabce733158e7
Kristian Garza <kj.garza@gmail.com>
2019-02-04 16:12:44 +0100

    change prefix totals method

--- app/controllers/prefixes_controller.rb
+++ app/controllers/prefixes_controller.rb
@@ -137,9 +137,12 @@ class PrefixesController < ApplicationController
 
   def totals
     page = { size: 25, number: 1 }
-    page_prov = { size: 2000, number: 1 }
+    # page_prov = { size: 2000, number: 1 }
 
-    ttl = Prefix.query("", page: page_prov, client_id: params[:client_id]).map do |prefix|    
+    prefixes = params[:client_id] ? Client.where(symbol: params[:client_id]).first.prefix_ids : Prefix.query("")
+
+    ttl = prefixes.map do |prefix|
+      prefix = prefix.respond_to?("downcase") ? prefix : prefix.prefix
       response = Doi.query("", client_id: params[:client_id], prefix: prefix, state: params[:state] || "",page: page)
       total = response.results.total
       states = total > 0 ? facet_by_key(response.response.aggregations.states.buckets) : nil
@@ -147,7 +150,7 @@ class PrefixesController < ApplicationController
       temporal[:this_month] = total > 0 ? facet_by_date(response.response.aggregations.this_month.buckets) : nil
       temporal[:this_year] = total > 0 ? facet_anual(response.response.aggregations.this_year.buckets) : nil
       temporal[:last_year] = total > 0 ? facet_anual(response.response.aggregations.last_year.buckets) : nil
-      id = client.symbol
+      id = prefix
       {id: id, title: id, count: total, states: states, temporal: temporal}
     end
     render json: ttl, status: :ok
