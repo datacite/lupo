c7adaa4f8c89e824a81dc7ba7cc67029c9bb66a0
Martin Fenner <martin.fenner@datacite.org>
2019-03-03 03:01:28 +0100

    use prov syntax. datacite/datacite#615

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -4,11 +4,31 @@ class ActivitySerializer
   set_type :activities
   set_id :request_uuid
   
-  attributes :doi, :username, :action, :changes, :created
+  attributes "prov:wasGeneratedBy", "prov:generatedAtTime", "prov:WasDerivedFrom", "prov:wasAttributedTo", "prov:type", :changes
 
   belongs_to :doi, record_type: :dois
 
-  attribute :doi do |object|
+  attribute "prov:WasDerivedFrom" do |object|
     object.uid
+  end
+
+  attribute "prov:wasAttributedTo" do |object|
+    object.username
+  end
+
+  attribute "prov:wasGeneratedBy" do |object|
+    url = Rails.env.production? ? "https://api.datacite.org" : "https://api.test.datacite.org"
+    "#{url}/activities/#{object.request_uuid}"
+  end
+
+  attribute "prov:generatedAtTime" do |object|
+    object.created
+  end
+
+  attribute "prov:type" do |object|
+    case object.action
+    when "create" then "prov:primarySource"
+    when "update" then "prov:Revision"
+    end
   end
 end
