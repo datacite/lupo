bcbfc07a1bcd532447f72dd18ed0c5fce401ff34
Martin Fenner <martin.fenner@datacite.org>
2019-03-11 21:39:07 +0100

    version bump

--- config/initializers/_version.rb
+++ config/initializers/_version.rb
@@ -1,7 +1,7 @@
 module Lupo
   class Application
     g = Git.open(Rails.root, :log => Logger.new(STDOUT))
-    VERSION = "2.3.43"
+    VERSION = "2.3.44"
     REVISION = g.object('HEAD').sha
   end
 end
