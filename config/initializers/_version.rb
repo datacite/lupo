517d23df619f139eb20c2216dbf7d9a140fd3297
Martin Fenner <martin.fenner@datacite.org>
2019-03-12 07:41:06 +0100

    auto-generate version string

--- config/initializers/_version.rb
+++ config/initializers/_version.rb
@@ -1,7 +1,7 @@
 module Lupo
   class Application
     g = Git.open(Rails.root, :log => Logger.new(STDOUT))
-    VERSION = "2.3.45"
+    VERSION = g.tags.map { |t| Gem::Version.new(t.name) }.sort.last.to_s
     REVISION = g.object('HEAD').sha
   end
 end
