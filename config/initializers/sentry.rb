730ec66e11383fd751c4ef0af3310418a192861d
Martin Fenner <martin.fenner@datacite.org>
2019-03-12 13:05:08 +0100

    ignore elasticsearch network errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,7 @@ Raven.configure do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.release = "lupo:" + Lupo::Application::VERSION
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+
+  # ignore 502 and 503 from Elasticsearch
+  config.excluded_exceptions += ['Elasticsearch::Transport::Transport::Errors::BadGateway', 'Elasticsearch::Transport::Transport::Errors::ServiceUnavailable']
 end
