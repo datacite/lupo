304909d5b435e124dd5bc85459508a3b4272e0f1
Martin Fenner <martin.fenner@datacite.org>
2019-03-12 12:27:30 +0100

    use application name + version tag for release. #214

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
   config.dsn = ENV["SENTRY_DSN"]
-  config.release = Lupo::Application::REVISION
+  config.release = "lupo:" + Lupo::Application::VERSION
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
