8ed26e6bc4a86ffe78d22e246d2023f64c8a0e49
Martin Fenner <martin.fenner@datacite.org>
2019-05-16 11:49:06 +0200

    use active record for elasticsearch results. #274

--- app/graphql/elasticsearch_loader.rb
+++ app/graphql/elasticsearch_loader.rb
@@ -6,7 +6,11 @@ class ElasticsearchLoader < GraphQL::Batch::Loader
   end
 
   def perform(ids)
-    @model.find_by_id(ids).results.each { |record| fulfill(record.uid, record) }
+    if @model.name == "Doi"
+      @model.find_by_id(ids).results.each { |record| fulfill(record.uid, record) }
+    else
+      @model.find_by_id(ids).records.each { |record| fulfill(record.uid, record) }
+    end
     ids.each { |id| fulfill(id, nil) unless fulfilled?(id) }
   end
 end
