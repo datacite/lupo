d697d5f76e6fbf053b99a50f07cfaa4ced48fa99
Martin Fenner <martin.fenner@datacite.org>
2019-05-17 07:41:46 +0200

    include events in usage reports. #260

--- app/graphql/types/usage_report_type.rb
+++ app/graphql/types/usage_report_type.rb
@@ -6,4 +6,14 @@ class UsageReportType < BaseObject
   field :id, ID, null: false, description: "Usage report ID"
   field :reporting_period, ReportingPeriodType, null: false, description: "Time period covered by the report"
   field :date_created, String, null: false, description: "Date information was created"
+  field :datasets, UsageReportDatasetConnectionWithMetaType, null: false, description: "Datasets included in usage report", connection: true, max_page_size: 100 do
+    argument :first, Int, required: false, default_value: 25
+  end
+
+  def datasets(**args)
+    ids = Event.query(nil, subj_id: object[:id], source_id: "datacite-usage").fetch(:data, []).map do |e|
+      doi_from_url(e[:obj_id])
+    end
+    ElasticsearchLoader.for(Doi).load_many(ids)
+  end
 end
