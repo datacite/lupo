fd9476cea152a98eebed09db73069a008b3b96ba
Martin Fenner <martin.fenner@datacite.org>
2019-05-18 12:28:55 +0200

    connection usage datasets. #260

--- app/graphql/types/dataset_type.rb
+++ app/graphql/types/dataset_type.rb
@@ -2,4 +2,15 @@
 
 class DatasetType < BaseObject
   implements DoiItem
+
+  field :usage_reports, DatasetUsageReportConnectionWithMetaType, null: false, description: "Usage reports for this dataset", connection: true, max_page_size: 100 do
+    argument :first, Int, required: false, default_value: 25
+  end
+
+  def usage_reports(**args)
+    ids = Event.query(nil, obj_id: object.id).fetch(:data, []).map do |e|
+      e[:subj_id]
+    end
+    UsageReport.find_by_id(ids, page: { number: 1, size: args[:first] }).fetch(:data, [])
+  end
 end
