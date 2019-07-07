51bab8b7e5bb8273c46bee1144cbdce002b0582b
Kristian Garza <kj.garza@gmail.com>
2019-07-07 16:39:36 +0200

    removed comments

--- app/models/concerns/batch_loader_helper.rb
+++ app/models/concerns/batch_loader_helper.rb
@@ -3,18 +3,10 @@ module BatchLoaderHelper
 
   class_methods do
     def load_doi(object)
-      # Doi.find_by_id(object.doi).results
-      # BatchLoader::Executor.clear_current
 
       BatchLoader.for(object.uuid).batch do |dois, loader|
 
         dois = object.doi
-        # dois = ["10.17876/musewide/dr.1/05555","10.0166/fk2.stagefigshare.6420615.v1","10.1234/rr763th5wp.1,10.1234/7rdf2ryxjn.1"]
-        # Doi.find_by_id(dois).results.each do |doi| 
-        #   puts doi.doi
-        #   loader.call(object.uuid, [doi]) 
-        # end
-
         results = Doi.find_by_id(dois).results
         loader.call(object.uuid, results) 
       end
--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -12,17 +12,6 @@ class EventSerializer
   attributes :subj_id, :obj_id, :source_id, :relation_type_id, :total, :message_action, :source_token, :license, :occurred_at, :timestamp
   
   has_many :dois, record_type: :dois, serializer: DoiSerializer, id_method_name: :uid do |object|
-    # logger = Logger.new(STDOUT)
-    # bmp = Benchmark.ms {
-    #   load_doi(object)
-    # }
-    # if bmp > 10000
-    #   logger.warn "[Benchmark Warning] batchloading " + bmp.to_s + " ms"
-    # else
-    #   logger.info "[Benchmark] batchloading " + bmp.to_s + " ms"
-    # end
-    # puts "ouside"
-    # puts object.uuid
     load_doi(object)
   end
   
