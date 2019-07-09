2d249a96b8f0574722482cdbd5ba464a48b4c729
Martin Fenner <martin.fenner@datacite.org>
2019-07-09 19:19:16 +0200

    remove whitespace

--- app/models/concerns/batch_loader_helper.rb
+++ app/models/concerns/batch_loader_helper.rb
@@ -3,9 +3,7 @@ module BatchLoaderHelper
 
   class_methods do
     def load_doi(object)
-
       BatchLoader.for(object.uuid).batch do |dois, loader|
-
         dois = object.doi
         results = Doi.find_by_id(dois).results
         loader.call(object.uuid, results) 
