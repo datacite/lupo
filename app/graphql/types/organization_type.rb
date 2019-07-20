1c273f64031a353c753e391bb910f652d9840aae
Martin Fenner <martin.fenner@datacite.org>
2019-07-20 07:14:28 +0200

    include affiliation connections to dois in graphql. #318

--- app/graphql/types/organization_type.rb
+++ app/graphql/types/organization_type.rb
@@ -15,4 +15,39 @@ class OrganizationType < BaseObject
   field :fund_ref, [String], null: true, description: "Crossref Funder ID identifiers for organization"
   field :wikidata, [String], null: true, description: "Wikidata identifiers for organization"
   field :grid, [String], null: true, description: "GRID identifiers for organization"
+
+  field :datasets, OrganizationDatasetConnectionWithMetaType, null: false, description: "Funded datasets", connection: true, max_page_size: 100 do
+    argument :first, Int, required: false, default_value: 25
+  end
+
+  field :publications, OrganizationPublicationConnectionWithMetaType, null: false, description: "Funded publications", connection: true do
+    argument :query, String, required: false
+    argument :first, Int, required: false, default_value: 25
+  end
+
+  field :softwares, OrganizationSoftwareConnectionWithMetaType, null: false, description: "Funded software", connection: true, max_page_size: 100 do
+    argument :first, Int, required: false, default_value: 25
+  end
+
+  def datasets(**args)
+    ids = Event.query(nil, obj_id: object[:id], citation_type: "Dataset-Organization").results.to_a.map do |e|
+      doi_from_url(e.subj_id)
+    end
+    
+    ElasticsearchLoader.for(Doi).load_many(ids)
+  end
+
+  def publications(**args)
+    ids = Event.query(nil, obj_id: object[:id], citation_type: "Organization-ScholarlyArticle").results.to_a.map do |e|
+      doi_from_url(e.subj_id)
+    end
+    ElasticsearchLoader.for(Doi).load_many(ids)
+  end
+
+  def softwares(**args)
+    ids = Event.query(nil, obj_id: object[:id], citation_type: "Organization-SoftwareSourceCode").results.to_a.map do |e|
+      doi_from_url(e.subj_id)
+    end
+    ElasticsearchLoader.for(Doi).load_many(ids)
+  end
 end
