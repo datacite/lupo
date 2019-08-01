e37cc31bc13cb66aa4f7e90025abe9db73d1540b
Kristian Garza <kj.garza@gmail.com>
2019-08-01 15:28:39 +0200

    citation year test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,14 +2,32 @@ require 'rails_helper'
 
 describe Event, :type => :model, vcr: true do
   before(:each) { allow(Time.zone).to receive(:now).and_return(Time.mktime(2015, 4, 8)) }
+  context "event" do
+    subject { create(:event) }
 
-  subject { create(:event) }
+    it { is_expected.to validate_presence_of(:subj_id) }
+    it { is_expected.to validate_presence_of(:source_token) }
+    it { is_expected.to validate_presence_of(:source_id) }
 
-  it { is_expected.to validate_presence_of(:subj_id) }
-  it { is_expected.to validate_presence_of(:source_token) }
-  it { is_expected.to validate_presence_of(:source_id) }
+    it "has subj" do
+      expect(subject.subj["date-published"]).to eq("2006-06-13T16:14:19Z")
+    end
+  end
 
-  it "has subj" do
-    expect(subject.subj["date-published"]).to eq("2006-06-13T16:14:19Z")
+  context "citation" do
+    subject { create(:event_for_datacite_related) }
+   
+    it "has citation_id" do
+      expect(subject.citation_id).to eq("https://doi.org/10.5061/dryad.47sd5/1-https://doi.org/10.5061/dryad.47sd5e/1")
+    end
+  
+    it "has citation_year" do
+      expect(subject.citation_year).to eq(2006)
+    end
+  
+    it "has published_dates" do
+      expect(subject.subj["datePublished"]).to eq("2006-06-13T16:14:19Z")
+      expect(subject.obj["datePublished"]).to be_nil
+    end
   end
 end
