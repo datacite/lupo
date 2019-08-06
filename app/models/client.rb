c6a1b47bda8f8f085428aaf24817241aa88351c7
Martin Fenner <martin.fenner@datacite.org>
2019-08-06 08:21:21 +0200

    properly index provider for clients and dois to show consortium summary stats. datacite/datacite#805

--- app/models/client.rb
+++ app/models/client.rb
@@ -107,7 +107,85 @@ class Client < ActiveRecord::Base
       indexes :cumulative_years, type: :integer, index: "false"
 
       # include parent objects
-      indexes :provider,      type: :object
+      indexes :provider,                       type: :object, properties: {
+        id: { type: :keyword },
+        uid: { type: :keyword },
+        symbol: { type: :keyword },
+        client_ids: { type: :keyword },
+        prefix_ids: { type: :keyword },
+        name: { type: :text, fields: { keyword: { type: "keyword" }, raw: { type: "text", "analyzer": "string_lowercase", "fielddata": true }} },
+        display_name: { type: :text, fields: { keyword: { type: "keyword" }, raw: { type: "text", "analyzer": "string_lowercase", "fielddata": true }} },
+        system_email: { type: :text, fields: { keyword: { type: "keyword" }} },
+        group_email: { type: :text, fields: { keyword: { type: "keyword" }} },
+        version: { type: :integer },
+        is_active: { type: :keyword },
+        year: { type: :integer },
+        description: { type: :text },
+        website: { type: :text, fields: { keyword: { type: "keyword" }} },
+        logo_url: { type: :text },
+        region: { type: :keyword },
+        focus_area: { type: :keyword },
+        organization_type: { type: :keyword },
+        member_type: { type: :keyword },
+        consortium_id: { type: :text, fields: { keyword: { type: "keyword" }, raw: { type: "text", "analyzer": "string_lowercase", "fielddata": true }} },
+        consortium_organization_ids: { type: :keyword },
+        country_code: { type: :keyword },
+        role_name: { type: :keyword },
+        cache_key: { type: :keyword },
+        joined: { type: :date },
+        twitter_handle: { type: :keyword },
+        ror_id: { type: :keyword },
+        billing_information: { type: :object, properties: {
+          postCode: { type: :keyword },
+          state: { type: :text},
+          organization: { type: :text},
+          department: { type: :text},
+          city: { type: :text },
+          country: { type: :text },
+          address: { type: :text }
+        } },
+        technical_contact: { type: :object, properties: {
+          email: { type: :text },
+          given_name: { type: :text},
+          family_name: { type: :text }
+        } },
+        secondary_technical_contact: { type: :object, properties: {
+          email: { type: :text },
+          given_name: { type: :text},
+          family_name: { type: :text }
+        } },
+        billing_contact: { type: :object, properties: {
+          email: { type: :text },
+          given_name: { type: :text},
+          family_name: { type: :text }
+        } },
+        secondary_billing_contact: { type: :object, properties: {
+          email: { type: :text },
+          given_name: { type: :text},
+          family_name: { type: :text }
+        } },
+        service_contact: { type: :object, properties: {
+          email: { type: :text },
+          given_name: { type: :text},
+          family_name: { type: :text }
+        } },
+        secondary_service_contact: { type: :object, properties: {
+          email: { type: :text },
+          given_name: { type: :text},
+          family_name: { type: :text }
+        } },
+        voting_contact: { type: :object, properties: {
+          email: { type: :text },
+          given_name: { type: :text},
+          family_name: { type: :text }
+        } },
+        created: { type: :date },
+        updated: { type: :date },
+        deleted_at: { type: :date },
+        cumulative_years: { type: :integer, index: "false" },
+        consortium: { type: :object },
+        consortium_organizations: { type: :object }
+      }
     end
   end
 
