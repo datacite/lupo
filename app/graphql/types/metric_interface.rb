6992af1bea679a7b80ac35aae0aa02f0af4722f6
Kristian Garza <kj.garza@gmail.com>
2019-08-28 09:48:28 +0200

    remove breakdown and rename variables
    Decide not to show any breakdown in stats in the tooltip https://docs.google.com/document/d/1StRUxV5_nsBQf64H6Os7WsjWw-rVaQAeef9GMAkPezs/edit#bookmark=id.u3lh6pf1zl4h

--- app/graphql/types/metric_interface.rb
+++ app/graphql/types/metric_interface.rb
@@ -3,46 +3,26 @@
 module MetricInterface
   include GraphQL::Schema::Interface
 
-  field :views_count, Integer, null: true, description: "The count of DOI views according to the COUNTER code of Practice for Research Data"
-  field :downloads_count, Integer, null: true, description: "The count of  DOI dowloands according to the COUNTER code of Practice for Research Data"
-  field :citations_count, Integer, null: true, description: "The count of DOI events that represents citations"
-  field :crossref_citations_count, Integer, null: true, description: "The count of DOI events that represents citations from Crossref"
-  field :datacite_citations_count, Integer, null: true, description: "The count of DOI events that represents citations from DataCite"
+  field :view_count, Integer, null: true, description: "The count of DOI views according to the COUNTER code of Practice for Research Data"
+  field :download_count, Integer, null: true, description: "The count of  DOI dowloands according to the COUNTER code of Practice for Research Data"
+  field :citation_count, Integer, null: true, description: "The count of DOI events that represents citations"
 
   def aggregation_results(**args)
     Event.query(nil, doi: doi_from_url(args[:id]), "page[size]": 0, aggregations: "metrics_aggregations", source_id: args[:source_id] || nil).response.aggregations
   end
 
-  def views_count
+  def view_count
     meta = aggregation_results(id: object.identifier).views.dois.buckets
     meta.first.fetch("total_by_type", {}).fetch("value", nil) if meta.any?
   end
 
-  def downloads_count
+  def download_count
     meta = aggregation_results(id: object.identifier).downloads.dois.buckets
     meta.first.fetch("total_by_type", {}).fetch("value", nil) if meta.any?
   end
 
-  def citations_count
+  def citation_count
     meta = aggregation_results(id: object.identifier).citations.dois.buckets
-    meta.first.fetch("unique_citations", {}).fetch("value", nil) if meta.any?
-  end
-
-  def crossref_citations_count
-    args = {
-      id: object.identifier,
-      source_id: "crossref"
-    }
-    meta = aggregation_results(args).citations.dois.buckets
-    meta.first.fetch("unique_citations", {}).fetch("value", nil) if meta.any?
-  end
-
-  def datacite_citations_count
-    args = {
-      id: object.identifier,
-      source_id: "datacite-related,datacite-crossref"
-    }
-    meta = aggregation_results(args).citations.dois.buckets
     meta.first.fetch("unique_citations", {}).fetch("value", nil) if meta.any?
   end
 end
