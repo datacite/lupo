0c1fdc0311cf49e541715872185f2f20f2c1b40c
Martin Fenner <martin.fenner@datacite.org>
2019-10-11 19:09:41 +0200

    address fields differently

--- app/graphql/types/event_data_edge.rb
+++ app/graphql/types/event_data_edge.rb
@@ -10,29 +10,29 @@ class EventDataEdge < GraphQL::Relay::Edge
 
   def event_data
     @event_data ||= begin
-      Event.query(nil, subj_id: self.node[:id], obj_id: self.parent[:id]).results.first.to_h
+      Event.query(nil, subj_id: self.node.id, obj_id: self.parent.id).results.first.to_h
     end
   end
 
   def source_id
-    self.node[:id]
+    self.node.id
   end
 
   def target_id
-    self.parent[:id]
+    self.parent.id
   end
 
   def source
-    event_data[:source_id].underscore.camelcase(:lower)
+    event_data.source_id.underscore.camelcase(:lower)
   end
 
   # We are switching subj and obj, and thus need to change direction of relation type
   def relation_type
-    rt = event_data[:relation_type_id].underscore.camelcase(:lower)
+    rt = event_data.relation_type_id.underscore.camelcase(:lower)
     RELATION_TYPES.fetch(rt, rt) 
   end
 
   def total
-    event_data[:total]
+    event_data.total
   end
 end
