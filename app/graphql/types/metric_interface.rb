dec26c5ebed63b9e7ebe90e95d753864523267b8
Kristian Garza <kj.garza@gmail.com>
2019-11-04 14:49:53 +0100

    added citations distribution for graphql

--- app/graphql/types/metric_interface.rb
+++ app/graphql/types/metric_interface.rb
@@ -8,6 +8,8 @@ module MetricInterface
   field :citation_count, Integer, null: true, description: "The count of DOI events that represents citations"
   field :reference_count, Integer, null: true, description: "The count of DOI events that represents references"
   field :relation_count, Integer, null: true, description: "The count of DOI events that represents relations"
+  field :citation_histogram, GraphQL::Types::JSON, null: true, description: "The citations distribution overtime"
+
   # field :citations_list, [String], null: true, description: "List of DOIS citing a given DOI"
   # field :referenceslist, [String], null: true, description: "List of DOIS that a given DOI references to"
   # field :relations_list, [String], null: true, description: "List of DOIS relations a given DOI has"
@@ -67,5 +69,17 @@ module MetricInterface
 
   def relations_aggs
     aggregation_results(id: object.identifier, aggregations: "citations_aggregations").relations.dois.buckets
+  end
+
+  def citation_histogram
+    hash = aggregation_results(id: object.identifier, aggregations: "citations_aggregations").citations_histogram
+
+    hash.dig('years', 'buckets').map do |h|
+      year = h['key']
+      {
+        'id' => year,
+        'sum' => h.dig('total_by_year', 'value') 
+      }
+    end
   end
 end
