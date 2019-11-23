a486f02c61744560af9df222fc7f08b52d291ba9
Martin Fenner <martin.fenner@datacite.org>
2019-11-23 08:39:49 +0100

    more flexible ES_HOST options

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,23 +3,23 @@
 require 'faraday_middleware'
 require 'faraday_middleware/aws_sigv4'
 
-if ENV['ES_HOST'] == "elasticsearch:9200"
-  config = {
-    host: ENV['ES_HOST'],
-    transport_options: {
-      request: { timeout: 30 }
-    }
-  }
-  Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['ES_HOST'], user: "elastic", password: ENV['ELASTIC_PASSWORD']) do |f|
-    f.adapter :excon
-  end
-else
+if ENV['ES_HOST'] == "elasticsearch.test.datacite.org" || ENV['ES_HOST'] == "elasticsearch.datacite.org"
   Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['ES_HOST'], port: '80', scheme: 'http') do |f|
     f.request :aws_sigv4,
       credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
       service: 'es',
       region: ENV['AWS_REGION']
 
+    f.adapter :excon
+  end
+else
+  # config = {
+  #   host: ENV['ES_HOST'],
+  #   transport_options: {
+  #     request: { timeout: 30 }
+  #   }
+  # }
+  Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['ES_HOST'], user: "elastic", password: ENV['ELASTIC_PASSWORD']) do |f|
     f.adapter :excon
   end
 end
