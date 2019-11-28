225bad9cd39808a2e33212cfd015bdd9b8adda3e
Kristian Garza <kj.garza@gmail.com>
2019-11-28 07:16:50 +0100

    remove relation

--- app/queries/events_query.rb
+++ app/queries/events_query.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class EventsQuery
-  attr_reader :relation
 
   include Facetable
 
@@ -17,12 +16,11 @@ class EventsQuery
      "is-referenced-by"
   ]
 
-  def initialize(relation = Event.new)
-    @relation = relation
+  def initialize()
   end
 
   def doi_citations(doi)
-    pid = relation.normalize_doi(doi)
+    pid = Event.new.normalize_doi(doi)
     query = "(subj_id:\"#{pid}\" AND (relation_type_id:#{PASSIVE_RELATION_TYPES.join(' OR relation_type_id:')})) OR (obj_id:\"#{pid}\" AND (relation_type_id:#{ACTIVE_RELATION_TYPES.join(' OR relation_type_id:')}))"
     results = Event.query(query, doi:doi, aggregations: "citation_count_aggregation", page: { size: 1, cursor: [] }).response.aggregations.citations.buckets
     results.any? ? results.first.total.value : 0
