ecc29337bcf251ce2d3e4fd7991f5f6adb475859
kjgarza <kgarza@datacite.org>
2020-01-27 21:38:37 +0100

    linting

--- app/queries/events_query.rb
+++ app/queries/events_query.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "pp"
+
 class EventsQuery
   include Facetable
   include Helpable
@@ -8,20 +10,20 @@ class EventsQuery
   ACTIVE_RELATION_TYPES = [
     "cites",
     "is-supplemented-by",
-    "references"
-  ]
+    "references",
+  ].freeze
 
   PASSIVE_RELATION_TYPES = [
-     "is-cited-by",
-     "is-supplement-to",
-     "is-referenced-by"
-  ]
+    "is-cited-by",
+    "is-supplement-to",
+    "is-referenced-by",
+  ].freeze
 
-  def initialize
-  end
+  def initialize; end
 
   def doi_citations(doi)
-    return nil unless doi.present?
+    return nil if doi.blank?
+
     pid = Event.new.normalize_doi(doi)
     query = "(subj_id:\"#{pid}\" AND (relation_type_id:#{PASSIVE_RELATION_TYPES.join(' OR relation_type_id:')})) OR (obj_id:\"#{pid}\" AND (relation_type_id:#{ACTIVE_RELATION_TYPES.join(' OR relation_type_id:')}))"
     results = Event.query(query, doi: doi, aggregations: "citation_count_aggregation", page: { size: 1, cursor: [] }).response.aggregations.citations.buckets
@@ -29,7 +31,8 @@ class EventsQuery
   end
 
   def citations_left_query(dois)
-    return nil unless dois.present?
+    return nil if dois.blank?
+
     pids = dois.split(",").map do |doi|
       Event.new.normalize_doi(doi)
     end.uniq
@@ -41,7 +44,8 @@ class EventsQuery
   end
 
   def citations_right_query(dois)
-    return nil unless dois.present?
+    return nil if dois.blank?
+
     pids = dois.split(",").map do |doi|
       Event.new.normalize_doi(doi)
     end.uniq
@@ -59,7 +63,8 @@ class EventsQuery
   end
 
   def citations_histogram(doi)
-    return {} unless doi.present?
+    return {} if doi.blank?
+
     pid = Event.new.normalize_doi(doi.downcase.split(",").first)
     query = "(subj_id:\"#{pid}\" AND (relation_type_id:#{PASSIVE_RELATION_TYPES.join(' OR relation_type_id:')})) OR (obj_id:\"#{pid}\" AND (relation_type_id:#{ACTIVE_RELATION_TYPES.join(' OR relation_type_id:')}))"
     results = Event.query(query, doi: doi, aggregations: "yearly_histogram_aggregation", page: { size: 1, cursor: [] }).response.aggregations
@@ -67,14 +72,16 @@ class EventsQuery
   end
 
   def doi_views(doi)
-    return nil unless doi.present?
+    return nil if doi.blank?
+
     query = "(relation_type_id:unique-dataset-investigations-regular AND source_id:datacite-usage)"
     results = Event.query(query, doi: doi, aggregations: "usage_count_aggregation", page: { size: 1, cursor: [] }).response.aggregations.usage.buckets
     results.any? ? results.first.dig("total_by_type", "value") : 0
   end
 
   def views(dois)
-    return {} unless dois.present?
+    return {} if dois.blank?
+
     query = "(relation_type_id:unique-dataset-investigations-regular AND source_id:datacite-usage)"
     results = Event.query(query, doi: dois, aggregations: "usage_count_aggregation", page: { size: 1, cursor: [] }).response.aggregations.usage.buckets
 
@@ -83,9 +90,9 @@ class EventsQuery
     end
   end
 
-
   def views_histogram(doi)
-    return {} unless doi.present?
+    return {} if doi.blank?
+
     doi = doi.downcase.split(",").first
     query = "(relation_type_id:unique-dataset-investigations-regular AND source_id:datacite-usage)"
     results = Event.query(query, doi: doi, aggregations: "monthly_histogram_aggregation", page: { size: 1, cursor: [] }).response.aggregations
@@ -93,14 +100,16 @@ class EventsQuery
   end
 
   def doi_downloads(doi)
-    return nil unless doi.present?
+    return nil if doi.blank?
+
     query = "(relation_type_id:unique-dataset-requests-regular AND source_id:datacite-usage)"
     results = Event.query(query, doi: doi, aggregations: "usage_count_aggregation", page: { size: 1, cursor: [] }).response.aggregations.usage.buckets
     results.any? ? results.first.dig("total_by_type", "value") : 0
   end
 
   def downloads(dois)
-    return {} unless dois.present?
+    return {} if dois.blank?
+
     query = "(relation_type_id:unique-dataset-requests-regular AND source_id:datacite-usage)"
     results = Event.query(query, doi: dois, aggregations: "usage_count_aggregation", page: { size: 1, cursor: [] }).response.aggregations.usage.buckets
 
@@ -110,11 +119,27 @@ class EventsQuery
   end
 
   def downloads_histogram(doi)
-    return {} unless doi.present?
+    return {} if doi.blank?
+
     doi = doi.downcase.split(",").first
     query = "(relation_type_id:unique-dataset-requests-regular AND source_id:datacite-usage)"
     results = Event.query(query, doi: doi, aggregations: "monthly_histogram_aggregation", page: { size: 1, cursor: [] }).response.aggregations
     facet_counts_by_year_month(results)
+  end
+
+  def views_and_downloads(dois)
+    return {} if dois.blank?
+
+    query = "(relation_type_id:unique-dataset-requests-regular AND source_id:datacite-usage) OR (relation_type_id:unique-dataset-investigations-regular AND source_id:datacite-usage)"
+    results = Event.query(query, doi: dois, aggregations: "multiple_usage_count_aggregation", page: { size: 1, cursor: [] }).response.aggregations
+
+    results.usage.buckets.map do |bucket|
+      views = bucket.relation_types.buckets.select { |item| item["key"] == "unique-dataset-investigations-regular" }.first
+      downloads = bucket.relation_types.buckets.select { |item| item["key"] == "unique-dataset-requests-regular" }.first
+      views_counts = views.nil? ? 0 : views.dig("total_by_type", "value")
+      downloads_counts = downloads.nil? ? 0 : downloads.dig("total_by_type", "value")
+      { id: doi_from_url(bucket[:key]), downloads: downloads_counts, views: views_counts }
+    end
   end
 
   def usage(doi)
