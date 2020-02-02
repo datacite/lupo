729abb50fddbb6f59b06e939f481b963abbbe2dc
Martin Fenner <martin.fenner@datacite.org>
2020-02-02 22:48:46 +0100

    only show metrics in meta if count > 0. #390

--- app/controllers/concerns/facetable.rb
+++ app/controllers/concerns/facetable.rb
@@ -34,10 +34,16 @@ module Facetable
     end
 
     def metric_facet_by_year(arr)
-      arr.map do |hsh|
-        { "id" => hsh["key_as_string"][0..3],
-          "title" => hsh["key_as_string"][0..3],
-          "count" => hsh.dig("metric_count", "value").to_i }
+      arr.reduce([]) do |sum, hsh|
+        if hsh.dig("metric_count", "value").to_i > 0
+          sum << { 
+            "id" => hsh["key_as_string"][0..3],
+            "title" => hsh["key_as_string"][0..3],
+            "count" => hsh.dig("metric_count", "value").to_i
+          }
+        end
+
+        sum
       end
     end
 
