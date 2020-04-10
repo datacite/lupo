5d51a6a915add774443002f059deacaa4a7d33bf
Martin Fenner <martin.fenner@datacite.org>
2020-04-10 12:40:31 +0200

    allow datetime

--- app/graphql/types/date_type.rb
+++ app/graphql/types/date_type.rb
@@ -3,6 +3,6 @@
 class DateType < BaseObject
   description "Information about dates"
 
-  field :date, GraphQL::Types::ISO8601Date, null: false, description: "Date information for this resource"
+  field :date, GraphQL::Types::ISO8601DateTime, null: false, description: "Date information for this resource"
   field :date_type, String, null: true, hash_key: "dateType", description: "The type of date"
 end
