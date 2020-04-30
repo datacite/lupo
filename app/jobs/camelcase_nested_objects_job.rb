74bfaa6da4649116c5191580114760fd0cce5d22
kjgarza <kgarza@datacite.org>
2020-04-30 11:48:37 +0200

    remove job that is not needed anymore

--- app/jobs/camelcase_nested_objects_job.rb
+++
@@ -1,7 +0,0 @@
-class CamelcaseNestedObjectsJob < ActiveJob::Base
-  queue_as :lupo_background
-
-  def perform(ids, options = {})
-    ids.each { |id| CamelcaseNestedObjectsByIdJob.perform_later(id, options) }
-  end
-end
