3a9836fc48439de76d604f7daff0cd414b08c36d
Martin Fenner <martin.fenner@datacite.org>
2020-04-30 10:29:04 +0200

    fix specs

--- spec/requests/provider_prefixes_spec.rb
+++ spec/requests/provider_prefixes_spec.rb
@@ -146,7 +146,7 @@ describe "Provider Prefixes", type: :request, elasticsearch: true do
         post '/provider-prefixes', not_valid_attributes, headers
 
         expect(last_response.status).to eq(422)
-        expect(json["errors"].first).to eq("source"=>"provider", "title"=>"Must exist")
+        expect(json["errors"].first).to eq("source"=>"provider", "title"=>"Can't be blank")
       end
     end
   end
