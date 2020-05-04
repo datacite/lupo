b1a2c4a19d87471de998d6542923489933946ac4
kjgarza <kgarza@datacite.org>
2020-05-04 13:30:00 +0200

    remove comments

--- db/seeds/development/consortium_transfer.seeds.rb
+++ db/seeds/development/consortium_transfer.seeds.rb
@@ -4,28 +4,14 @@ fail "Seed tasks can only be used in the development enviroment" if Rails.env.pr
 
 after "development:base" do
 
-## ranks
-# Lieutenant of Supplies
-# Master of Ships
-# Commander of the Law
-# General of Warlords
-# Supreme Master
-# Supreme General
-# Wing Commander
-# Lawbringer
-# Lord General
-# Paladin
-
-  provider = Provider.where(symbol: "GENERAL").first || FactoryBot.create(:provider, symbol: "GENERAL")
-  client = Client.where(symbol: "PALADIN").first || FactoryBot.create(:client, provider: provider, symbol: "PALADIN", password: ENV["MDS_PASSWORD"]) 
-  if Prefix.where(uid: "10.14456").blank?
-    prefix = FactoryBot.create(:prefix, uid: "10.14456") 
-    # FactoryBot.create(:client_prefix, client_id: client.id, prefix_id: prefix.id) 
+  provider = Provider.where(symbol: "QUECHUA").first || FactoryBot.create(:provider, symbol: "QUECHUA")
+  client = Client.where(symbol: "QUECHUA.TEXT").first || FactoryBot.create(:client, provider: provider, symbol: "QUECHUA.TEXT", password: ENV["MDS_PASSWORD"]) 
+  if Prefix.where(uid: "10.14459").blank?
+    prefix = FactoryBot.create(:prefix, uid: "10.14459")
+    FactoryBot.create(:provider_prefix, provider_id: provider.symbol, prefix_id: prefix.uid) 
+    FactoryBot.create(:client_prefix, client_id: client.symbol, prefix_id: prefix.uid)
   end
   dois = FactoryBot.create_list(:doi, 10, client: client, state: "findable")
   FactoryBot.create_list(:event_for_datacite_related, 3, obj_id: dois.first.doi)
   FactoryBot.create_list(:event_for_datacite_usage, 2, obj_id: dois.first.doi)
 end
-
-
-
