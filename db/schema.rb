efdfadaed1a3e3e20de014d3c000bfd3f7d05413
Martin Fenner <martin.fenner@datacite.org>
2020-05-22 19:38:19 +0200

    Revert "split migrations into two"
    This reverts commit 53394962f506dd237129172ec038288cf86945ce.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_085039) do
+ActiveRecord::Schema.define(version: 2020_05_16_052834) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", limit: 191, null: false
