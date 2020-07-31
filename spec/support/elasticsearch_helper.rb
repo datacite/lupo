72ff374d0953b9d0236baa687c4aa7a4f4ae5176
Martin Fenner <martin.fenner@datacite.org>
2020-07-31 09:54:11 +0200

    use other_doi elasticsearch index in testing

--- spec/support/elasticsearch_helper.rb
+++ spec/support/elasticsearch_helper.rb
@@ -1,5 +1,5 @@
 ## https://github.com/elastic/elasticsearch-ruby/issues/462
-SEARCHABLE_MODELS = [Client, Provider, DataciteDoi, Doi, Event, Activity, Prefix, ClientPrefix, ProviderPrefix]
+SEARCHABLE_MODELS = [Client, Provider, DataciteDoi, OtherDoi, Doi, Event, Activity, Prefix, ClientPrefix, ProviderPrefix]
 
 RSpec.configure do |config|
   config.around :example, elasticsearch: true do |example|
@@ -7,7 +7,7 @@ RSpec.configure do |config|
       if model.name == "DataciteDoi" || model.name == "OtherDoi"
         model.create_template
       end
-      
+
       Elasticsearch::Model.client.indices.delete index: "#{model.index_name}_v1" if Elasticsearch::Model.client.indices.exists? index: "#{model.index_name}_v1"
       Elasticsearch::Model.client.indices.delete index: "#{model.index_name}_v2" if Elasticsearch::Model.client.indices.exists? index: "#{model.index_name}_v2"
 
