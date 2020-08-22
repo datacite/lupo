6613d95c8eee792a312dd39a804ebe82d2f15c8d
Martin Fenner <martin.fenner@datacite.org>
2020-08-22 09:09:59 +0200

    fix elasticsearch custom alias deletion

--- app/models/concerns/indexable.rb
+++ app/models/concerns/indexable.rb
@@ -492,12 +492,10 @@ module Indexable
     # delete alias
     def delete_alias(options={})
       alias_name = options[:alias] || self.index_name
-      index_name = options[:index] || self.index_name + "_v1"
-      alternate_index_name = options[:index] || self.index_name + "_v2"
+      index_name = (options[:index] || self.index_name) + "_v1"
+      alternate_index_name = (options[:index] || self.index_name) + "_v2"
 
       client = Elasticsearch::Model.client
-
-      self.__elasticsearch__.delete_index!(index: alias_name) if self.__elasticsearch__.index_exists?(index: alias_name)
 
       # indexes in DOI model are aliased from DataciteDoi and OtherDoi models
       # TODO switch to DataciteDoi index
