499ea8223343aea5080b71fcbed51ce7583776e1
Martin Fenner <martin.fenner@datacite.org>
2020-09-03 22:13:58 +0200

    make geolocation point latitude and longitude optional

--- app/graphql/types/geolocation_point_type.rb
+++ app/graphql/types/geolocation_point_type.rb
@@ -3,6 +3,6 @@
 class GeolocationPointType < BaseObject
   description "A point contains a single longitude-latitude pair."
 
-  field :point_longitude, Float, null: false, hash_key: "pointLongitude", description: "Longitudinal dimension of point."
-  field :point_latitude, Float, null: false, hash_key: "pointLatitude", description: "Latitudinal dimension of point."
+  field :point_longitude, Float, null: true, hash_key: "pointLongitude", description: "Longitudinal dimension of point."
+  field :point_latitude, Float, null: true, hash_key: "pointLatitude", description: "Latitudinal dimension of point."
 end
