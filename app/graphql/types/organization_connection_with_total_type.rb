39fab86160627d7381d21b40f0cc27a0e686c9cd
Martin Fenner <martin.fenner@datacite.org>
2020-10-18 18:08:53 +0200

    add ror statistics by year. #660

--- app/graphql/types/organization_connection_with_total_type.rb
+++ app/graphql/types/organization_connection_with_total_type.rb
@@ -4,10 +4,29 @@ class OrganizationConnectionWithTotalType < BaseConnection
   edge_type(OrganizationEdgeType)
   field_class GraphQL::Cache::Field
 
+  # data from GRID taken on Oct 18, 2020 https://grid.ac/downloads
+  # using latest release in any given year, starting with end of 2017,
+  # right before ROR was launched in January 2018
+  YEARS = [
+    { "id" => "2017", "title" => "2017", "count" => 80248 },
+    { "id" => "2018", "title" => "2018", "count" => 11392 },
+    { "id" => "2019", "title" => "2019", "count" => 6179 },
+  ]
+
   field :total_count, Integer, null: false, cache: true
+  field :years, [FacetType], null: true, cache: true
   field :types, [FacetType], null: true, cache: true
   field :countries, [FacetType], null: true, cache: true
   field :person_connection_count, Integer, null: false, cache: true
+
+  def years
+    count = YEARS.reduce(0) do |sum, i|
+      sum += i["count"]
+      sum
+    end
+    this_year = object.total_count > count ? { "id" => "2020", "title" => "2020", "count" => object.total_count - count } : nil
+    this_year ? YEARS << this_year : YEARS
+  end
 
   def total_count
     object.total_count
