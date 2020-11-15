95fa06cfc897bf65effe662bb2575cf9a81c6a6f
Martin Fenner <martin.fenner@datacite.org>
2020-11-15 14:23:34 +0100

    identifier fields should be optional

--- app/graphql/types/identifier_type.rb
+++ app/graphql/types/identifier_type.rb
@@ -3,7 +3,7 @@
 class IdentifierType < BaseObject
   description "Information about identifiers"
 
-  field :identifier_type, String, null: false, hash_key: "identifierType", description: "The type of identifier."
-  field :identifier, String, null: false, description: "The value of the identifier."
+  field :identifier_type, String, null: true, hash_key: "identifierType", description: "The type of identifier."
+  field :identifier, String, null: true, description: "The value of the identifier."
   field :identifier_url, String, null: true, hash_key: "identifierUrl", description: "The url of the identifier."
 end
