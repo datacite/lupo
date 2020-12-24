c5ddd0835ee0b10df1731703b6eb47afd8228a52
Martin Fenner <martin.fenner@datacite.org>
2020-12-24 15:46:32 +0100

    fix index_document error message. #690

--- app/jobs/index_background_job.rb
+++ app/jobs/index_background_job.rb
@@ -11,6 +11,6 @@ class IndexBackgroundJob < ApplicationJob
   def perform(obj)
     response = obj.__elasticsearch__.index_document
     # Rails.logger.error "[Elasticsearch] Error indexing id #{response["_id"]} in index #{response["_index"]}" if response["result"] != "created"
-    Rails.logger.error "[Elasticsearch] Error #{response.inspect}" if response["result"] != "created"
+    Rails.logger.error "[Elasticsearch] Error #{response.inspect}" unless %w(created updated).include?(response["result"])
   end
 end
