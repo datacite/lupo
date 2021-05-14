6eb0f455cf3754b6c32073ad7136b2b2b85663c1
Suzanne Vogt <suzanne.vogt@datacite.org>
2021-05-14 02:01:10 -0400

    Issue #712 - Increase request entity size. (Lint fixes.)

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -1,4 +1,6 @@
-require 'compressed_requests'
+# frozen_string_literal: true
+
+require "compressed_requests"
 
 Rails.application.configure do
   config.middleware.insert_before Rack::Head, CompressedRequests
