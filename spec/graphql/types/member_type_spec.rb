9b1741b5e3768dcff994807432a31139b26aa677
jrhoads <jrhoads@datacite.org>
2022-01-06 05:02:50 -0500

    Update more specs for the new year

--- spec/graphql/types/member_type_spec.rb
+++ spec/graphql/types/member_type_spec.rb
@@ -242,7 +242,7 @@ describe MemberType do
 
       expect(response.dig("data", "member", "prefixes", "totalCount")).to eq(3)
       expect(response.dig("data", "member", "prefixes", "years")).to eq(
-        [{ "count" => 3, "id" => "2021" }],
+        [{ "count" => 3, "id" => "2022" }],
       )
       expect(response.dig("data", "member", "prefixes", "nodes").length).to eq(
         3,
