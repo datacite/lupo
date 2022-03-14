7df417e63a275dfa711ebe5a52c2083698fccc20
Suzanne Vogt <suzanne.vogt@datacite.org>
2022-03-13 22:55:03 -0400

    Salesforce - doi_estimate_year_one field. (Allow doi_estimate to be set for any org type.)

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -840,24 +840,20 @@ class Provider < ApplicationRecord
   end
 
   def doi_estimate_field
-    if member_type == "consortium_organization"
-      begin
-        num = Integer(doi_estimate)
-        if num < 0
-          errors.add(
-            :doi_estimate,
-            :doi_estimate_invalid,
-            value: "The doi_estimate must be a nonnegative integer.",
-          )
-        end
-      rescue
-        errors.add(
-          :doi_estimate,
-          :doi_estimate_invalid,
-          value: "The doi_estimate must be a nonnegative integer.",
-        )
-      end
+    num = Integer(doi_estimate)
+    if num < 0
+      errors.add(
+        :doi_estimate,
+        :doi_estimate_invalid,
+        value: "The doi_estimate must be a nonnegative integer.",
+      )
     end
+  rescue
+    errors.add(
+      :doi_estimate,
+      :doi_estimate_invalid,
+      value: "The doi_estimate must be a nonnegative integer.",
+    )
   end
 
   # attributes to be sent to elasticsearch index
@@ -947,11 +943,7 @@ class Provider < ApplicationRecord
       self.billing_information = {} if billing_information.blank?
       self.consortium_id = nil unless member_type == "consortium_organization"
       self.non_profit_status = "non-profit" if non_profit_status.blank?
-      if member_type == "consortium_organization"
-        self.doi_estimate = doi_estimate.to_i
-      else
-        self.doi_estimate = 0
-      end
+      self.doi_estimate = doi_estimate.to_i
 
       # custom filename for attachment as data URLs don't support filenames
       if logo_content_type.present?
