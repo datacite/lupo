83af9b4869c4d4b6108d6bfd37dd1aa770c8cb7a
Mike Bennett <mbennett@datacite.org>
2022-07-04 12:17:20 +0100

    Convern function to preserve fields qstring

--- app/controllers/concerns/fieldable.rb
+++ app/controllers/concerns/fieldable.rb
@@ -11,5 +11,13 @@ module Fieldable
       fields.each { |k, v| fields[k] = v.to_s.split(",") }
       fields
     end
+
+    def fields_hash_from_params(params)
+      fields = params.to_unsafe_h.dig(:fields)
+      return nil unless fields.is_a?(Hash)
+
+      fields.each { |k, v| fields[k] = v }
+      fields
+    end
   end
 end
