fd1e38986dd44c322ec914defc548a0e2c9002e1
Mike Bennett <mbennett@datacite.org>
2022-08-02 10:18:13 +0100

    Schema.db updates (activestorage tables -> utf8)

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_07_07_142354) do
 
-  create_table "active_storage_attachments", charset: "latin1", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2022_07_07_142354) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "latin1", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
     t.string "key", limit: 191, null: false
     t.string "filename", limit: 191, null: false
     t.string "content_type", limit: 191
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2022_07_07_142354) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "latin1", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 2022_07_07_142354) do
     t.index ["symbol"], name: "symbol", unique: true
   end
 
-  create_table "audits", charset: "latin1", force: :cascade do |t|
+  create_table "audits", charset: "utf8mb4", force: :cascade do |t|
     t.integer "auditable_id"
     t.string "auditable_type"
     t.integer "associated_id"
@@ -307,7 +307,7 @@ ActiveRecord::Schema.define(version: 2022_07_07_142354) do
     t.index ["uid"], name: "index_provider_prefixes_on_uid", length: 128
   end
 
-  create_table "reference_repositories", charset: "latin1", force: :cascade do |t|
+  create_table "reference_repositories", charset: "utf8mb4", force: :cascade do |t|
     t.string "client_id"
     t.string "re3doi"
     t.datetime "created_at", null: false
