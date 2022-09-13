9a19f08d9f6ad003df47dc43b5eeff686093a6ee
Suzanne Vogt <suzanne.vogt@datacite.org>
2022-09-12 22:24:56 -0400

    Prefix bug - clean up

--- spec/support/database_cleaner_helper.rb
+++ spec/support/database_cleaner_helper.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation, { pre_count: true }
   end
-  
+
   config.before(:each) { DatabaseCleaner.start }
 
   config.after(:each) { DatabaseCleaner.clean }
