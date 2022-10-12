7b14e773c8ee9d3452bcce5314f7c60bc03b0557
Suzanne Vogt <suzanne.vogt@datacite.org>
2022-10-12 11:01:56 -0400

    Prefix bug - address review comment.

--- spec/requests/repositories_spec.rb
+++ spec/requests/repositories_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "pp"
 
 describe RepositoriesController, type: :request, elasticsearch: true do
   let(:ids) { clients.map(&:uid).join(",") }
