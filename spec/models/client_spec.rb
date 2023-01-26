39caf88e95fcb6d8d8e1fc9c09d1a32efee7b45f
Richard Hallett <rhallett@datacite.org>
2023-01-26 15:48:15 +0100

    Better fix for annoying test

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -417,9 +417,7 @@ describe Client, type: :model do
 
     it "should show all cumulative years" do
       client = create(:client, provider: provider)
-      expect(client.cumulative_years).to eq(
-        [2_015, 2_016, 2_017, 2_018, 2_019, 2_020, 2_021, 2_022, 2_023],
-      )
+      expect(client.cumulative_years).to eq(2015..Date.today.year).to_a
     end
 
     it "should show years before deleted" do
