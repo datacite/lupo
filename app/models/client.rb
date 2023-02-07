82ead0f744ea8d66f263b13a4bc6655ed9614b87
svogt0511 <suzanne.vogt@datacite.org>
2023-02-07 00:25:22 -0500

    Partial revert - prefix-bug-1

--- app/models/client.rb
+++ app/models/client.rb
@@ -98,7 +98,7 @@ class Client < ApplicationRecord
   before_validation :set_defaults
   before_create { self.created = Time.zone.now.utc.iso8601 }
   before_save { self.updated = Time.zone.now.utc.iso8601 }
-  after_create_commit :assign_prefix
+  after_create :assign_prefix
   after_create_commit :create_reference_repository
   after_update_commit :update_reference_repository
   after_destroy_commit :destroy_reference_repository
