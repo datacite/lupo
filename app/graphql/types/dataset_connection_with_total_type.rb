063eb6d12ce657d31ef775b71c9fe97d3798d31d
jrhoads <jrhoads@datacite.org>
2023-05-10 21:38:36 -0400

    Apply WorksFacetInterface to dataset_connection

--- app/graphql/types/dataset_connection_with_total_type.rb
+++ app/graphql/types/dataset_connection_with_total_type.rb
@@ -2,18 +2,11 @@
 
 class DatasetConnectionWithTotalType < BaseConnection
   edge_type(DatasetEdgeType)
+  implements Interfaces::WorkFacetsInterface
+
   field_class GraphQL::Cache::Field
 
   field :total_count, Integer, null: false, cache: true
-  field :published, [FacetType], null: true, cache: true
-  field :registration_agencies, [FacetType], null: true, cache: true
-  field :repositories, [FacetType], null: true, cache: true
-  field :affiliations, [FacetType], null: true, cache: true
-  field :fields_of_science, [FacetType], null: true, cache: true
-  field :authors, [FacetType], null: true, cache: true
-  field :licenses, [FacetType], null: true, cache: true
-  field :languages, [FacetType], null: true, cache: true
-
   field :dataset_connection_count, Integer, null: false, cache: true
   field :publication_connection_count, Integer, null: false, cache: true
   field :software_connection_count, Integer, null: false, cache: true
@@ -23,72 +16,6 @@ class DatasetConnectionWithTotalType < BaseConnection
 
   def total_count
     object.total_count
-  end
-
-  def published
-    if object.aggregations.published
-      facet_by_range(object.aggregations.published.buckets)
-    else
-      []
-    end
-  end
-
-  def registration_agencies
-    if object.aggregations.registration_agencies
-      facet_by_registration_agency(
-        object.aggregations.registration_agencies.buckets
-      )
-    else
-      []
-    end
-  end
-
-  def repositories
-    if object.aggregations.clients
-      facet_by_combined_key(object.aggregations.clients.buckets)
-    else
-      []
-    end
-  end
-
-  def authors
-    if object.aggregations.authors
-      facet_by_authors(object.aggregations.authors.buckets)
-    else
-      []
-    end
-  end
-
-  def affiliations
-    if object.aggregations.affiliations
-      facet_by_combined_key(object.aggregations.affiliations.buckets)
-    else
-      []
-    end
-  end
-
-  def licenses
-    if object.aggregations.licenses
-      facet_by_license(object.aggregations.licenses.buckets)
-    else
-      []
-    end
-  end
-
-  def fields_of_science
-    if object.aggregations.fields_of_science
-      facet_by_fos(object.aggregations.fields_of_science.subject.buckets)
-    else
-      []
-    end
-  end
-
-  def languages
-    if object.aggregations.languages
-      facet_by_language(object.aggregations.languages.buckets)
-    else
-      []
-    end
   end
 
   def dataset_connection_count
