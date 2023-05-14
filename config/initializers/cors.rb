15f92dc38ca306ccb5482931dadb364106632a76
kjgarza <kgarza@datacite.org>
2023-05-14 13:26:44 +0200

    remove cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,11 +6,11 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ""  # replace with your actual trusted sites
-    resource "graphql",
-      headers: "*",
-      methods: [:get, :post, :head, :options]
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins ""  # replace with your actual trusted sites
+#     resource "graphql",
+#       headers: "*",
+#       methods: [:get, :post, :head, :options]
+#   end
+# end
