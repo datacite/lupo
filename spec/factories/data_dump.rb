93048d821327bf878b4de3f37f479e4f1c9cbf85
Mike Bennett <mbennett@datacite.org>
2023-07-13 10:52:18 +0100

    Add a factory for an incomplete data dump (datacite/1868)

--- spec/factories/data_dump.rb
+++ spec/factories/data_dump.rb
@@ -11,5 +11,12 @@ FactoryBot.define do
     checksum { Faker::Crypto.sha256}
     created_at { Faker::Time.backward(1, :morning) }
     updated_at { Faker::Time.backward(1, :evening) }
+    aasm_state { :complete }
+  end
+  factory :data_dump_incomplete do
+    uid { Faker::Internet.password(8).downcase }
+    scope { "metadata" }
+    description { "Test Metadata Data Dump Factory creation - incomplete" }
+    created_at { Faker::Time.backward(2, :morning) }
   end
 end
