8a0a9c6bf93b3288a010c2a1c948ff727ab2da72
Mike Bennett <mbennett@datacite.org>
2023-07-13 12:56:29 +0100

    Update factory to use Faker for more attributes so it can be used to generate multiple instances (datacite/1868)

--- spec/factories/data_dump.rb
+++ spec/factories/data_dump.rb
@@ -4,13 +4,17 @@ require "faker"
 
 FactoryBot.define do
   factory :data_dump do
+    transient do
+      year { Faker::Number.within(2010..2021).to_s }
+    end
+
     uid { Faker::Internet.password(8).downcase }
     scope { "metadata" }
     description { "Test Metadata Data Dump Factory creation"}
-    start_date { "01/01/2022" }
-    end_date { "31/12/2022" }
-    records { 12345 }
-    checksum { Faker::Crypto.sha256}
+    start_date { "#{year}-01-01" }
+    end_date { "#{year}-12-31" }
+    records { Faker::Number.within(5_000_000..50_000_000) }
+    checksum { Faker::Crypto.sha256 }
     created_at { Faker::Time.backward(1, :morning) }
     updated_at { Faker::Time.backward(1, :evening) }
     aasm_state { :complete }
