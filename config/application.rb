4c061eca5473a5d361166713a93cb1285a61561e
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2023-09-28 18:14:16 +0200

    remove commented out code

--- config/application.rb
+++ config/application.rb
@@ -26,10 +26,10 @@ end
 # load ENV variables from container environment if json file exists
 # see https://github.com/phusion/baseimage-docker#envvar_dumps
 env_json_file = "/etc/container_environment.json"
-# if File.exist?(env_json_file)
-#   env_vars = JSON.parse(File.read(env_json_file))
-#   env_vars.each { |k, v| ENV[k] = v }
-# end
+if File.exist?(env_json_file)
+  env_vars = JSON.parse(File.read(env_json_file))
+  env_vars.each { |k, v| ENV[k] = v }
+end
 
 # default values for some ENV variables
 ENV["APPLICATION"] ||= "client-api"
