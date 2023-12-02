20e11e63fcb5e91da50446b5c7b4c840006412d0
jrhoads <jrhoads@datacite.org>
2023-12-02 09:00:39 -0500

    Add more fields to the related_dois indexed info

--- app/models/doi/indexer/related_identifier_denormalizer.rb
+++ app/models/doi/indexer/related_identifier_denormalizer.rb
@@ -10,18 +10,25 @@ module Doi::Indexer
 
     def to_hash
       %w[
-        resource_type_id
+        client_id
         doi
         organization_id
+        person_id
+        resource_type_id
+        resource_type_id_and_name
       ].index_with { |method_name| send(method_name) }
     end
 
     delegate :resource_type_id, to: :related_doi
+    delegate :resource_type_id_and_name, to: :related_doi
     delegate :organization_id, to: :related_doi
+    delegate :person_id, to: :related_doi
+    delegate :client_id, to: :related_doi
 
     def doi
       @related_doi.doi.downcase
     end
+
   end
 end
 
