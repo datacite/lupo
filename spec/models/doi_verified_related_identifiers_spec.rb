b749c97301b641d211514664616e114d7ec9be10
jrhoads <jrhoads@datacite.org>
2023-12-02 09:01:56 -0500

    Add tests for related_dois

--- spec/models/doi_verified_related_identifiers_spec.rb
+++ spec/models/doi_verified_related_identifiers_spec.rb
@@ -23,12 +23,38 @@ describe Doi, type: :model, vcr: true, elasticsearch: true do
             "relatedIdentifierType": "DOI",
             "relationType": "HasPart",
             "resourceTypeGeneral": "OutputManagementPlan",
+          },
+          {
+            "relatedIdentifier": target_doi.doi,
+            "relatedIdentifierType": "DOI",
+            "relationType": "Cites",
+            "resourceTypeGeneral": "Text",
           }
         ])
     end
 
     it "indexes related_dois" do
-      expect(doi.related_dois.first[:doi]).to eq(target_doi.doi)
+      expect(doi.related_dois.first["doi"]).to eq(target_doi.doi.downcase)
+    end
+
+    it "indexes related doi's client_id" do
+      expect(doi.related_dois.first["client_id"]).to eq(target_doi.client_id)
+    end
+
+    it "indexes related doi's person_id" do
+      expect(doi.related_dois.first["person_id"]).to eq(target_doi.person_id)
+    end
+
+    it "does not index related doi's claimed resource_type_id" do
+      expect(doi.related_dois.first["resource_type_id"]).not_to eq("output_management_plan")
+    end
+
+    it "indexes related doi's true resource_type_id" do
+      expect(doi.related_dois.first["resource_type_id"]).to eq("dataset")
+    end
+
+    it "indexes all relations to the related doi" do
+      expect(doi.related_dois.first["relation_type"]).to eq(["has_part", "cites"])
     end
 
   end
