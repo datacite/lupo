4ea937c4831e11d1a697bde13e77efea110d5688
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-01-23 11:00:38 -0500

    Review comments.

--- app/graphql/types/doi_item.rb
+++ app/graphql/types/doi_item.rb
@@ -67,7 +67,7 @@ module DoiItem
         description:
           "The name of the entity that holds, archives, publishes prints, distributes, releases, issues, or produces the resource",
         deprecation_reason:
-          "This field will change structure on February 25, 2024.  Applications that use it will need to be updated to the new structure by then.  Please see support.datacite.org for details."
+          "This field will change structure on February 27, 2024.  Applications that use it will need to be updated to the new structure by then.  For details: https://support.datacite.org/docs/publisher-changes-in-schema-45"
   field :subjects,
         [SubjectType],
         null: true,
