0d602c055c3387c4d67ca6efa444bfed41fae391
kaysiz <kuda.siziva@datacite.org>
2024-02-09 14:44:20 +0200

    fix rubocop issue

--- db/migrate/20240209120111_add_missing_indexes_to_allocator.rb
+++ db/migrate/20240209120111_add_missing_indexes_to_allocator.rb
@@ -2,7 +2,7 @@
 
 class AddMissingIndexesToAllocator < ActiveRecord::Migration[6.1]
   def change
-    add_index :allocator, :deleted_at, name: 'index_allocator_deleted_at'
-    add_index :allocator, :role_name, name: 'index_allocator_role_name'
+    add_index :allocator, :deleted_at, name: "index_allocator_deleted_at"
+    add_index :allocator, :role_name, name: "index_allocator_role_name"
   end
 end
