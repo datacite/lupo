ebb6ce52e8ae8cd28f0a4a8cdd8269fda00429dc
kaysiz <kuda.siziva@datacite.org>
2024-02-14 11:11:26 +0200

    fix rubocp issue

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2024_02_14_083153) do
-
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", null: false
