87814d77cdcee07090c94a6d79300926fe4f1c26
kaysiz <kuda.siziva@datacite.org>
2024-02-14 15:06:31 +0200

    fix rubocop issue

--- db/migrate/20240214123201_add_indexes_to_media.rb
+++ db/migrate/20240214123201_add_indexes_to_media.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIndexesToMedia < ActiveRecord::Migration[6.1]
   def change
     add_index :media, [:dataset, :created], name: "index_media_dataset_created"
