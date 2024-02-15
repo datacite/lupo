9746f99885e4f4afa76acb4d09ecfd23b11535dc
kaysiz <kuda.siziva@datacite.org>
2024-02-15 11:32:17 +0200

    fix rubocop issue

--- db/migrate/20240215091610_add_indexes_to_metadata.rb
+++ db/migrate/20240215091610_add_indexes_to_metadata.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIndexesToMetadata < ActiveRecord::Migration[6.1]
   def change
     add_index :metadata, [:dataset, :created], name: "index_metadata_dataset_created"
