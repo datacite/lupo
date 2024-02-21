cc6083ec3ecd457158f303478617fff19ad874aa
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-02-21 17:13:49 +0200

    disable shoryuken workers

--- vendor/docker/shoryuken.sh
+++ vendor/docker/shoryuken.sh
@@ -3,5 +3,5 @@ cd /home/app/webapp
 exec 2>&1
 # If AWS_REGION is set and not explicitly disabled with DISABLE_QUEUE_WORKER, start shoryuken
 if [ -n "$AWS_REGION" ] && [ -z "$DISABLE_QUEUE_WORKER" ]; then
-  exec /sbin/setuser app bundle exec shoryuken -R -C config/shoryuken.yml
+  # exec /sbin/setuser app bundle exec shoryuken -R -C config/shoryuken.yml
 fi
