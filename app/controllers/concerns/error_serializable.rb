570533c8e25f8b76d4f7c4a1f70bdc90d6ebfbf9
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-02-28 21:48:59 +0200

    rework error serialization

--- app/controllers/concerns/error_serializable.rb
+++ app/controllers/concerns/error_serializable.rb
@@ -6,18 +6,8 @@ module ErrorSerializable
   included do
     def serialize_errors(errors, options = {})
       return nil if errors.nil?
-
-      arr = errors.reduce([]) do |sum, err|
-          src = err.attribute
-          sum << {
-            source: err.attribute,
-            uid: options[:uid],
-            title: err.message,
-          }
-          sum
-        end
-
-      { errors: arr }.to_json
+      errors_arr = errors.map { |err| { source: err.attribute, title: err.message.capitalize } }.uniq
+      return { errors: errors_arr }.to_json
     end
   end
 end
