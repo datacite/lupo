7bbff228c4082af3e623326fc5c41daa280a1130
Suzanne Vogt <suzanne.vogt@datacite.org>
2024-02-29 17:43:52 -0500

    Add schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_09_120111) do
+ActiveRecord::Schema.define(version: 2024_02_29_195058) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", limit: 191, null: false
