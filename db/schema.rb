99ddcdea61c92b6092868a3a927eaa4e060edddb
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2024-04-03 10:45:53 +0200

    Add/Remove Indexes - Contacts Table (#1122)
    * Add missing indexes on contacts table
    
    * fix rubocp issue
    
    * fix rubocp issue
    
    * Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2024_02_29_195058) do
-
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", limit: 191, null: false
     t.string "record_type", null: false
@@ -137,6 +135,9 @@ ActiveRecord::Schema.define(version: 2024_02_29_195058) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_contacts_deleted_at"
+    t.index ["email"], name: "index_contacts_email"
+    t.index ["uid"], name: "index_contacts_uid"
   end
 
   create_table "datacentre", charset: "utf8", force: :cascade do |t|
