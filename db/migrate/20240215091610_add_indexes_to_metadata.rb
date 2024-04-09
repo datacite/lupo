3466df1203838e386120a0f34fb2e0197b0c7de3
Wendel Fabian Chinsamy <wendelwebdev@gmail.com>
2024-04-09 10:34:59 +0200

    Revert "Disable depature gem for metadata migration (#1165)"
    This reverts commit 5f40e64e118021c1009ce4f3395409c2046311f1.

--- db/migrate/20240215091610_add_indexes_to_metadata.rb
+++ db/migrate/20240215091610_add_indexes_to_metadata.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class AddIndexesToMetadata < ActiveRecord::Migration[6.1]
-  disable_departure!
-
   def change
     add_index :metadata, [:dataset, :created], name: "index_metadata_dataset_created"
   end
