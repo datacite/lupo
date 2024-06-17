6ed092f4327d763a39621cb6c39c461adb2ed859
Bryceson Laing <bryceson.laing@datacite.org>
2024-06-17 16:25:00 +0100

    Logging context

--- app/graphql/mutations/create_claim.rb
+++ app/graphql/mutations/create_claim.rb
@@ -12,8 +12,7 @@ class CreateClaim < BaseMutation
     Rails.logger.error "RESOLVING CLAIM"
     if doi.blank? || context[:current_user].blank?
       Rails.logger.error "CLAIM IS NULL"
-      Rails.logger.error doi
-      Rails.logger.error context[:current_user]
+      Rails.logger.error context
       return { claim: nil, errors: [] }
     end
 
