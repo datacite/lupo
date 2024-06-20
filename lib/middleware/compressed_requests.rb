f68c3bf7f14107320c1376c98747d304e6bf66da
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-06-20 15:52:43 +0200

    add middleware logging

--- lib/middleware/compressed_requests.rb
+++ lib/middleware/compressed_requests.rb
@@ -28,8 +28,14 @@ module Middleware
         env["rack.input"] = StringIO.new(extracted)
       end
 
-      status, headers, response = @app.call(env)
-      [status, headers, response]
+      begin
+        status, headers, response = @app.call(env)
+        [status, headers, response]
+      rescue => err
+        Rails.logger.err(err)
+        Raven.capture_exception(err)
+        [406, {}, [{"status": 406, "title": err.message}.to_json]]
+      end
     end
 
     def decode(input, content_encoding)
