271ca354b2e3694bc21818c526f2ddfd7a9183f2
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-06-20 15:53:56 +0200

    add credential logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -100,6 +100,10 @@ class ApplicationController < ActionController::API
 
   # based on https://github.com/nsarno/knock/blob/master/lib/knock/authenticable.rb
   def type_and_credentials_from_request_headers
+    Rails.logger.info("orcid_claim: inside credentials read")
+    request.headers.each do |key, value|
+      puts"#{key}: #{value}"
+    end
     request.headers["Authorization"]&.split
   end
 
