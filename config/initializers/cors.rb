d813a194db22458d746b95b2cdc26c70bc48010b
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-06-20 15:06:01 +0200

    fix linting issues and remove log statements

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*',
+    origins "*"
+    resource "*",
       headers: :any,
       expose: ["Authorization"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
