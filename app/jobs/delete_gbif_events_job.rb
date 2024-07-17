a6edf8f1ae685e691e2bc442b15849b7089eebaa
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-07-17 17:11:03 +0200

    appease the rubocop overlords

--- app/jobs/delete_gbif_events_job.rb
+++ app/jobs/delete_gbif_events_job.rb
@@ -3,13 +3,11 @@
 class DeleteGbifEventsJob < ApplicationJob
   queue_as :lupo_background
 
-  def perform(id, options={})
-    begin
-      event = Event.find_by(uuid: id)
+  def perform(id, options = {})
+    event = Event.find_by(uuid: id)
 
-      event.destroy if event.present?
-    rescue => err
-      Rails.logger.info("#{options[:label]}: event delete error: #{err.message}")
-    end
+    event.destroy if event.present?
+  rescue => err
+    Rails.logger.info("#{options[:label]}: event delete error: #{err.message}")
   end
 end
