9279e252f81bb62f2f015243e406b1588193c58c
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-07-23 14:32:12 +0200

    change orphaned events task

--- app/jobs/delete_orphaned_gbif_events_job.rb
+++ app/jobs/delete_orphaned_gbif_events_job.rb
@@ -3,14 +3,15 @@
 class DeleteOrphanedGbifEventsJob < ApplicationJob
   queue_as :lupo_background
 
-  def perform(env, query)
+  def perform(env, options)
     label = "DeleteOrphanedGbifEventsJob_#{Time.now.utc.strftime("%d%m%Y%H%M%S")}"
 
     Rails.logger.info("#{label}: index_name: #{env}")
+    Rails.logger.info("#{label}: query: #{query}")
 
-    response = Event.delete_by_query(index: env, query: query)
+    # response = Event.delete_by_query(index: env, query: options[:query])
 
-    Rails.logger.info(response.to_json)
+    # Rails.logger.info(response.to_json)
   rescue => err
     Rails.logger.info("#{label}: #{err.message}")
   end
