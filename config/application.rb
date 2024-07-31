5cea2884d94bf06546c15688ddc0890ceec52061
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-07-31 15:10:56 +0200

    remove datadog ip trace log

--- config/application.rb
+++ config/application.rb
@@ -171,9 +171,6 @@ module Lupo
     # use batch loader
     config.middleware.use BatchLoader::Middleware
 
-    # add the client ip address to the datadog trace
-    config.middleware.use Middleware::DatadogIpMiddleware
-
     # set Active Job queueing backend
     config.active_job.queue_adapter = ENV["AWS_REGION"] ? :shoryuken : :inline
 
