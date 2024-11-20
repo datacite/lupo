9555997f15d0b9462b3b3c787c655943f3f11ada
Bryceson Laing <bryceson.laing@datacite.org>
2024-11-20 11:24:52 -0600

    add spec for agency in dois search response

--- spec/requests/datacite_dois/datacite_dois_spec.rb
+++ spec/requests/datacite_dois/datacite_dois_spec.rb
@@ -456,6 +456,29 @@ describe DataciteDoisController, type: :request, vcr: true do
     end
   end
 
+  describe "GET /dois search with agency values", prefix_pool_size: 1 do
+    let!(:dois) { create_list(:doi, 10, client: client, aasm_state: "findable") }
+
+    before do
+      clear_doi_index
+      import_doi_index
+    end
+
+    it "returns agency values when flag is set" do
+      get "/dois?include_other_registration_agencies=true", nil, headers
+
+      expect(last_response.status).to eq(200)
+      expect(json.dig("data", 0, "attributes", "agency")).to eq("datacite")
+    end
+
+    it "does not returns agency values when flag isn't set" do
+      get "/dois", nil, headers
+
+      expect(last_response.status).to eq(200)
+      expect(json.dig("data", 0, "attributes")).to_not have_key("agency")
+    end
+  end
+
   describe "GET /dois with client-type filter", prefix_pool_size: 3 do
     let!(:dois) { create_list(:doi, 10, client: client, aasm_state: "findable", version_info: "testtag") }
     let(:client_igsn_id_catalog) { create(:client, provider: provider, client_type: "igsnCatalog") }
