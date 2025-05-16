c315065cbd71102d51ca88202b5d990d28069bd9
Richard Hallett <richard@richardhallett.com>
2025-05-16 15:05:28 +0200

    Add check for existing bucket on test setup

--- spec/support/objectstore_helper.rb
+++ spec/support/objectstore_helper.rb
@@ -14,17 +14,23 @@ def clean_bucket
   bucket.delete
 end
 
+def create_bucket
+  puts("Creating metadata bucket")
+  bucket_name = ENV["METADATA_STORAGE_BUCKET_NAME"]
+  s3 = Aws::S3::Resource.new(region: ENV["AWS_REGION"])
+  s3.create_bucket(bucket: bucket_name)
+
+rescue Aws::Errors::ServiceError => e
+  puts("Can't create bucket: #{e.message}")
+end
+
 RSpec.configure do |config|
   config.before(:suite) do
-    puts("Creating metadata bucket")
-
-    bucket_name = ENV["METADATA_STORAGE_BUCKET_NAME"]
-    s3 = Aws::S3::Resource.new(region: ENV["AWS_REGION"])
-    s3.create_bucket(bucket: bucket_name)
+    create_bucket
   end
 
   # This is to clean up beteween tests metadata documents stored in the objectstore i.e. minio
   config.after(:suite) do
-    clean_bucket
+    # clean_bucket
   end
 end
