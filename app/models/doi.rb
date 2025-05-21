8efb13881fa8222e168257118da521a83e64ea7e
codycooperross <50597551+codycooperross@users.noreply.github.com>
2025-05-21 14:44:44 -0400

    Implements JSON Schema for titles

--- app/models/doi.rb
+++ app/models/doi.rb
@@ -109,16 +109,22 @@ class Doi < ApplicationRecord
   validates_presence_of :doi
   validates_presence_of :url, if: Proc.new { |doi| doi.is_registered_or_findable? }
 
-  json_schema_validation = {
-    message: ->(errors) { errors },
-    schema: PUBLISHER_JSON_SCHEMA
-  }
-
   def validate_publisher_obj?(doi)
     doi.validatable? && doi.publisher_obj? && !(doi.publisher_obj.blank? || doi.publisher_obj.all?(nil))
   end
 
-  validates :publisher_obj, if: ->(doi) { validate_publisher_obj?(doi) }, json: json_schema_validation
+  def validate_field?(field_name)
+    validatable? && self[field_name].present?
+  end
+
+  def self.json_schema_validation(schema_name)
+    {
+      message: ->(errors) { errors },
+      schema: Rails.root.join("app", "models", "schemas", "doi", "#{schema_name}.json")
+    }
+  end
+
+  validates :publisher_obj, if: ->(doi) { validate_publisher_obj?(doi) }, json: json_schema_validation("publisher")
 
   # from https://www.crossref.org/blog/dois-and-matching-regular-expressions/ but using uppercase
   validates_format_of :doi, with: /\A10\.\d{4,5}\/[-._;()\/:a-zA-Z0-9*~$=]+\z/, on: :create
@@ -131,6 +137,7 @@ class Doi < ApplicationRecord
   validate :check_dates, if: :dates?
   validate :check_rights_list, if: :rights_list?
   validate :check_titles, if: :titles?
+  validates :titles, if: proc { |doi| validate_field?(:titles) }, json: json_schema_validation("titles")
   validate :check_descriptions, if: :descriptions?
   validate :check_types, if: :types?
   validate :check_container, if: :container?
