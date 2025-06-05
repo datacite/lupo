8d44f55f81282b4433982a0b70cf07299f7d9d01
jrhoads <jrhoads@datacite.org>
2025-06-05 17:24:38 +0200

    Appease Rubocop

--- app/services/sqs_message_service.rb
+++ app/services/sqs_message_service.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 class SqsMessageService
   def self.call(body, options = {})
     sqs = Aws::SQS::Client.new # Assumes Aws.config is set globally
-    
+
     queue_name_prefix = ENV["SQS_PREFIX"].present? ? ENV["SQS_PREFIX"] : Rails.env
     target_queue_name = "#{queue_name_prefix}_#{options[:queue_name]}"
-    
+
     effective_queue_url = if Aws.config[:stub_responses]
-                            # If stubbing is active, always construct a well-formed dummy URL.
-                            # The actual result of sqs.get_queue_url is unreliable when stubbed (returns "String").
-                            dummy_url = "https://sqs.#{Aws.config[:region]}.amazonaws.com/000000000000/#{target_queue_name}"
-                            # Call get_queue_url for its potential side effects or future compatibility, but ignore its direct return for the URL.
-                            sqs.get_queue_url(queue_name: target_queue_name) 
-                            dummy_url
-                          else
-                            # For real calls, use the result from get_queue_url
-                            sqs.get_queue_url(queue_name: target_queue_name).queue_url
-                          end
-    
+      # If stubbing is active, always construct a well-formed dummy URL.
+      # The actual result of sqs.get_queue_url is unreliable when stubbed (returns "String").
+      dummy_url = "https://sqs.#{Aws.config[:region]}.amazonaws.com/000000000000/#{target_queue_name}"
+      # Call get_queue_url for its potential side effects or future compatibility, but ignore its direct return for the URL.
+      sqs.get_queue_url(queue_name: target_queue_name)
+      dummy_url
+    else
+      # For real calls, use the result from get_queue_url
+      sqs.get_queue_url(queue_name: target_queue_name).queue_url
+    end
+
     # Default shoryuken_class if not provided
     shoryuken_class = options.fetch(:shoryuken_class, "DoiImportWorker")
 
@@ -29,7 +31,7 @@ class SqsMessageService
       },
       message_body: body.to_json,
     }
-    
+
     sqs.send_message(message_options)
   end
 end
--- config/initializers/aws_sdk_test_setup.rb
+++ config/initializers/aws_sdk_test_setup.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 # config/initializers/aws_sdk_test_setup.rb
 if Rails.env.test? && defined?(Aws)
   aws_test_config = {
-    region: ENV.fetch('AWS_REGION', 'us-stubbed-1'),
-    credentials: Aws::Credentials.new('DUMMY_ACCESS_KEY_ID', 'DUMMY_SECRET_ACCESS_KEY'),
+    region: ENV.fetch("AWS_REGION", "us-stubbed-1"),
+    credentials: Aws::Credentials.new("DUMMY_ACCESS_KEY_ID", "DUMMY_SECRET_ACCESS_KEY"),
     stub_responses: true
   }
   Aws.config.update(aws_test_config) # Aws.config will be a Hash
