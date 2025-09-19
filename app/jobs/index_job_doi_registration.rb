cdd3876c957c1e00f45b4f0336fa21ddf6054acf
jrhoads <jrhoads@datacite.org>
2025-09-19 14:06:44 +0200

    feat: Index dois in inactive index in index sync is enabled

--- app/jobs/index_job_doi_registration.rb
+++ app/jobs/index_job_doi_registration.rb
@@ -10,8 +10,19 @@ class IndexJobDoiRegistration < ApplicationJob
     Rails.logger.error error.message
   end
 
+  def index_sync_enabled?(obj)
+    if Rails.env.test?
+      return false
+    end
+    obj.respond_to?(:index_sync_enabled?) && obj.index_sync_enabled?
+  end
+
   def perform(obj)
     response = obj.__elasticsearch__.index_document
     Rails.logger.error "[Elasticsearch] Error #{response.inspect}" unless %w(created updated).include?(response["result"])
+    if index_sync_enabled?(obj)
+      response2 = obj.__elasticsearch__.index_document(index: obj.inactive_index)
+      Rails.logger.error "[Elasticsearch] Error #{response2.inspect}" unless %w(created updated).include?(response2["result"])
+    end
   end
 end
