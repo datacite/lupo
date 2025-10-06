7c141ec85bbbda465744e85f2baf0d2004b3665a
jrhoads <jrhoads@datacite.org>
2025-10-06 12:40:14 +0200

    refactor: Remove redundant index sync logic from background job

--- app/jobs/index_background_job.rb
+++ app/jobs/index_background_job.rb
@@ -9,20 +9,8 @@ class IndexBackgroundJob < ApplicationJob
               Elasticsearch::Transport::Transport::Error do |error|
     Rails.logger.error error.message
   end
-
-  def index_sync_enabled?(obj)
-    if Rails.env.test?
-      return false
-    end
-    obj.respond_to?(:index_sync_enabled?) && obj.index_sync_enabled?
-  end
-
   def perform(obj)
     response = obj.__elasticsearch__.index_document
     Rails.logger.error "[Elasticsearch] Error #{response.inspect}" unless %w(created updated).include?(response["result"])
-    if index_sync_enabled?(obj)
-      response2 = obj.__elasticsearch__.index_document(index: obj.inactive_index)
-      Rails.logger.error "[Elasticsearch] Error #{response2.inspect}" unless %w(created updated).include?(response2["result"])
-    end
   end
 end
