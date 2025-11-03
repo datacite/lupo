e9fbb134333c306626033f876932352bf5d6f774
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2025-11-03 17:11:44 +0200

    Add some staging testing code

--- app/jobs/other_doi_job.rb
+++ app/jobs/other_doi_job.rb
@@ -4,8 +4,16 @@ class OtherDoiJob < ApplicationJob
   queue_as :events_other_doi_job
 
   def perform(data, options = {})
+    # to test in staging
+    Rails.logger.info("#######################")
+    Rails.logger.info("the other doi job has run")
+    Rails.logger.info("#######################")
+    Rails.logger.info(data.inspect)
+    Rails.logger.info("#######################")
     event = Event.new(subj_id: data["subj_id"], obj_id: data["obj_id"])
     ids = event.dois_to_import
-    ids.each { |id| OtherDoiByIdJob.perform_later(id, options) }
+    Rails.logger.info(ids.inspect)
+    Rails.logger.info("#######################")
+    # ids.each { |id| OtherDoiByIdJob.perform_later(id, options) }
   end
 end
