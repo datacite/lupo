85ae2488eee68ad67be867a21238175a3792e2a6
kudakwashe SIziva <kuda.siziva@datacite.org>
2025-11-11 16:32:41 +0100

    fix failing graphql tests

--- spec/graphql/types/label_type_spec.rb
+++ spec/graphql/types/label_type_spec.rb
@@ -6,7 +6,7 @@ describe LabelType do
   describe "fields" do
     subject { described_class }
 
-    it { is_expected.to have_field(:code).of_type(!types.ID) }
+    it { is_expected.to have_field(:code).of_type("ID!") }
     it { is_expected.to have_field(:name).of_type("String") }
   end
 end
--- spec/graphql/types/repository_type_spec.rb
+++ spec/graphql/types/repository_type_spec.rb
@@ -11,10 +11,10 @@ describe RepositoryType do
   describe "fields" do
     subject { described_class }
 
-    it { is_expected.to have_field(:uid).of_type(!types.ID) }
+    it { is_expected.to have_field(:uid).of_type("ID!") }
     it { is_expected.to have_field(:type).of_type("String!") }
-    it { is_expected.to have_field(:clientId).of_type(types.ID) }
-    it { is_expected.to have_field(:re3dataDoi).of_type(types.ID) }
+    it { is_expected.to have_field(:clientId).of_type("ID") }
+    it { is_expected.to have_field(:re3dataDoi).of_type("ID") }
     it { is_expected.to have_field(:name).of_type("String!") }
     it { is_expected.to have_field(:alternateName).of_type("[String!]") }
     it { is_expected.to have_field(:description).of_type("String") }
