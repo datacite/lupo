05b301e5d38aad88623e36d3b10f930d0d630c41
Suzanne Vogt <suzanne.vogt@datacite.org>
2025-12-08 16:13:48 -0500

    Comment out language validation.  There is a problem with it.

--- spec/graphql/types/dissertation_type_spec.rb
+++ spec/graphql/types/dissertation_type_spec.rb
@@ -11,13 +11,12 @@ describe DissertationType do
   end
 
   describe "query dissertations", elasticsearch: true do
-    # Commented out Language because there is an error on create.
     let!(:datacite_dissertations) do
       create_list(
         :doi,
         2,
         types: { "resourceTypeGeneral" => "Text", "resourceType" => "Thesis" },
-        # language: "de",
+        language: "de",
         aasm_state: "findable",
       )
     end
@@ -169,13 +168,12 @@ describe DissertationType do
   end
 
   describe "query dissertations by license", elasticsearch: true do
-    # Commented out Language because there is an error on create.
     let!(:datacite_dissertations) do
       create_list(
         :doi,
         2,
         types: { "resourceTypeGeneral" => "Text", "resourceType" => "Thesis" },
-        # language: "de",
+        language: "de",
         aasm_state: "findable",
       )
     end
@@ -227,23 +225,22 @@ describe DissertationType do
       }"
     end
 
-    # Temporarily Commenting this out - Due to language validation problem?
-    # it "returns all dissertations" do
-    #   response = LupoSchema.execute(query).as_json
-    #
-    #   expect(response.dig("data", "dissertations", "totalCount")).to eq(2)
-    #   expect(
-    #     response.dig("data", "dissertations", "registrationAgencies"),
-    #   ).to eq([{ "count" => 2, "id" => "datacite", "title" => "DataCite" }])
-    #   expect(response.dig("data", "dissertations", "languages")).to eq(
-    #     [{ "count" => 2, "id" => "de", "title" => "German" }],
-    #   )
-    #   expect(response.dig("data", "dissertations", "nodes").length).to eq(2)
-    #   # expect(response.dig("data", "dissertations", "nodes", 0, "id")).to eq(@dois.first.identifier)
-    #   expect(
-    #     response.dig("data", "dissertations", "nodes", 0, "registrationAgency"),
-    #   ).to eq("id" => "datacite", "name" => "DataCite")
-    # end
+    it "returns all dissertations" do
+      response = LupoSchema.execute(query).as_json
+    
+      expect(response.dig("data", "dissertations", "totalCount")).to eq(2)
+      expect(
+        response.dig("data", "dissertations", "registrationAgencies"),
+      ).to eq([{ "count" => 2, "id" => "datacite", "title" => "DataCite" }])
+      expect(response.dig("data", "dissertations", "languages")).to eq(
+        [{ "count" => 2, "id" => "de", "title" => "German" }],
+      )
+      expect(response.dig("data", "dissertations", "nodes").length).to eq(2)
+      # expect(response.dig("data", "dissertations", "nodes", 0, "id")).to eq(@dois.first.identifier)
+      expect(
+        response.dig("data", "dissertations", "nodes", 0, "registrationAgency"),
+      ).to eq("id" => "datacite", "name" => "DataCite")
+    end
   end
 
   describe "query dissertations by person", elasticsearch: true do
