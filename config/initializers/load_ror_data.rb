17a1194561804fea7d4fdab0d219d37c40b1eeda
Ashwini Sukale <ashwini.sukale@datacite.org>
2026-01-23 13:58:20 +0530

    Fixed load error

--- config/initializers/load_ror_data.rb
+++ config/initializers/load_ror_data.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require Rails.root.join("app/services/ror_mapping_loader")
+
 if defined?(Shoryuken) && ENV["DISABLE_QUEUE_WORKER"].blank?
   require "aws-sdk-s3"
   require "fileutils"
