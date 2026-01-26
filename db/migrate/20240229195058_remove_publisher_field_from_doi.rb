f588745bc460943773cfcdc0b5d18ae743b78365
kudakwashe SIziva <kuda.siziva@datacite.org>
2026-01-26 10:49:20 +0100

    disable percona for doi migration

--- db/migrate/20240229195058_remove_publisher_field_from_doi.rb
+++ db/migrate/20240229195058_remove_publisher_field_from_doi.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class RemovePublisherFieldFromDoi < ActiveRecord::Migration[6.1]
+  disable_departure!
   def change
     remove_column :dataset, :publisher, :string
   end
