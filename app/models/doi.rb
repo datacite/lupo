2267c7b5ebd9020bfa1a68052fca958d3ce807a1
Suzanne Vogt <suzanne.vogt@datacite.org>
2026-02-06 10:19:16 -0500

    Changing relationTypeInformation from ES/OS 'keyword' type field to 'text' type field.

--- app/models/doi.rb
+++ app/models/doi.rb
@@ -300,12 +300,12 @@ class Doi < ApplicationRecord
         schemeUri: { type: :keyword },
         schemeType: { type: :keyword },
         resourceTypeGeneral: { type: :keyword },
-        relationTypeInformation: { type: :keyword },
+        relationTypeInformation: { type: :text },
       }
       indexes :related_items,                       type: :object, properties: {
         relatedItemType: { type: :keyword },
         relationType: { type: :keyword },
-        relationTypeInformation: { type: :keyword },
+        relationTypeInformation: { type: :text },
         relatedItemIdentifier: { type: :object, properties: {
           relatedItemIdentifier: { type: :keyword, normalizer: "keyword_lowercase" },
           relatedItemIdentifierType: { type: :keyword },
