5c415ee0a23664caf3a27e88a79b012d86132d62
Suzanne Vogt <suzanne.vogt@datacite.org>
2026-02-06 14:05:47 -0500

    Debugging output.

--- app/jobs/datacite_doi_batch_enqueue_job.rb
+++ app/jobs/datacite_doi_batch_enqueue_job.rb
@@ -5,8 +5,12 @@ class DataciteDoiBatchEnqueueJob < ApplicationJob
   queue_as :lupo_queue_batches_datacite_doi
 
   def perform(start_id, end_id, batch_size: 50, index: nil)
+    # GOT HERE!!!!
+    Rails.logger.info "ZZZZ: Enqueuing DataciteDoiImportInBulkJob for DataciteDois with IDs #{start_id}-#{end_id} in batches of #{batch_size} to index #{index}."
     ids = DataciteDoi.where(type: "DataciteDoi", id: start_id..end_id).pluck(:id)
     ids.each_slice(batch_size) do |batch_ids|
+      # GOT HERE!!!!
+      Rails.logger.info "ZZZZ: Enqueuing DataciteDoiImportInBulkJob for batch of DataciteDois with IDs #{batch_ids.first}-#{batch_ids.last} to index #{index}."
       DataciteDoiImportInBulkJob.perform_later(batch_ids, index: index)
     end
   end
--- app/jobs/datacite_doi_import_in_bulk_job.rb
+++ app/jobs/datacite_doi_import_in_bulk_job.rb
@@ -4,6 +4,8 @@ class DataciteDoiImportInBulkJob < ApplicationJob
   queue_as :lupo_import
 
   def perform(ids, options = {})
+    # GOT HERE!!!!
+    Rails.logger.info "ZZZZ: Starting DataciteDoiImportInBulkJob for DataciteDois with IDs #{ids.first}-#{ids.last} to index #{options[:index]}."
     DataciteDoi.import_in_bulk(ids, options)
   end
 end
