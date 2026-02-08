d3180e731017d7edbf8a313c2fea476a0165bb09
Suzanne Vogt <suzanne.vogt@datacite.org>
2026-02-08 16:18:13 -0500

    Issue due to upgrade? Presence of both positional parameters and keyed parameters mixed with upgrade to ruby 3 caused 'ArgumentError: wrong number of arguments (given 3, expected 2)' during reindexing of DOIS

--- app/jobs/datacite_doi_batch_enqueue_job.rb
+++ app/jobs/datacite_doi_batch_enqueue_job.rb
@@ -4,13 +4,9 @@
 class DataciteDoiBatchEnqueueJob < ApplicationJob
   queue_as :lupo_queue_batches_datacite_doi
 
-  def perform(start_id, end_id, batch_size: 50, index: nil)
-    # GOT HERE!!!!
-    Rails.logger.info "ZZZZ: Enqueuing DataciteDoiImportInBulkJob for DataciteDois with IDs #{start_id}-#{end_id} in batches of #{batch_size} to index #{index}."
+  def perform(start_id, end_id, batch_size = 50, index = nil )
     ids = DataciteDoi.where(type: "DataciteDoi", id: start_id..end_id).pluck(:id)
     ids.each_slice(batch_size) do |batch_ids|
-      # GOT HERE!!!!
-      Rails.logger.info "ZZZZ: Enqueuing DataciteDoiImportInBulkJob for batch of DataciteDois with IDs #{batch_ids.first}-#{batch_ids.last} to index #{index}."
       DataciteDoiImportInBulkJob.perform_later(batch_ids, index: index)
     end
   end
