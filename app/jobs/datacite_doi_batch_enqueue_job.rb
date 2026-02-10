4c23b5e30542ecc0f2a753cd58443a1b69c9b83a
Suzanne Vogt <suzanne.vogt@datacite.org>
2026-02-10 10:34:39 -0500

    Shoryuken - use all keyword arguments.

--- app/jobs/datacite_doi_batch_enqueue_job.rb
+++ app/jobs/datacite_doi_batch_enqueue_job.rb
@@ -4,7 +4,7 @@
 class DataciteDoiBatchEnqueueJob < ApplicationJob
   queue_as :lupo_queue_batches_datacite_doi
 
-  def perform(start_id, end_id, batch_size: 50, index: nil)
+  def perform(start_id:, end_id:, batch_size: 50, index: nil)
     ids = DataciteDoi.where(type: "DataciteDoi", id: start_id..end_id).pluck(:id)
     ids.each_slice(batch_size) do |batch_ids|
       DataciteDoiImportInBulkJob.perform_later(batch_ids, index: index)
