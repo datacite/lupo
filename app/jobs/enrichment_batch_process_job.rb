0035b0e1bfd3f21e2ab847f87c05b05706794f65
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-02-12 11:37:24 +0200

    Finish up the batch process job

--- app/jobs/enrichment_batch_process_job.rb
+++ app/jobs/enrichment_batch_process_job.rb
@@ -1,38 +1,78 @@
 # frozen_string_literal: true
 
 class EnrichmentBatchProcessJob < ApplicationJob
+  include ErrorSerializable
+
   queue_as :enrichment_batch_process_job
 
   def perform(lines)
     log_prefix = "EnrichmentBathProcessJob"
-    Rails.logger.info("#{log_prefix}: Job started")
 
+    # We will process the lines in parallel to speed up ingestion.
     Parallel.each(lines, in_threads: 10) do |line|
       parsed_line = JSON.parse(line)
 
+      # We only create enrichments for DOIs that exist and which have an agency of 'datacite'.
       doi = Doi.find_by(doi: parsed_line["doi"], agency: "datacite")
 
       if doi.blank?
-        Rails.logger.error(
-          "#{log_prefix}: Doi #{parsed_line["doi"]} does not exist")
-      else
-        enrichment = Enrichment.new(
-          doi: parsed_line["doi"],
-          contributors: parsed_line["contributors"],
-          resources: parsed_line["resources"],
-          field: parsed_line["field"],
-          action: parsed_line["action"],
-          original_value: parsed_line["originalValue"],
-          enriched_value: parsed_line["enrichedValue"]
-        )
-
-        unless enrichment.save
-          Rails.logger.warn(
-            "#{log_prefix}: Enrichment failed to save: #{enrichment.errors.full_messages.join(";")}")
-        end
+        Rails.logger.error("#{log_prefix}: Doi #{parsed_line["doi"]} does not exist")
+        next
       end
 
-      Rails.logger.info("#{log_prefix}: Job completed")
+      # We set the only_validate flag on the DOI model to true such that we
+      # ensure that validation functions as expected when not persisting the record.
+      doi.only_validate = true
+
+      enrichment = Enrichment.new(
+        doi: "#{parsed_line["doi"]}",
+        contributors: parsed_line["contributors"],
+        resources: parsed_line["resources"],
+        field: parsed_line["field"],
+        action: parsed_line["action"],
+        original_value: parsed_line["originalValue"],
+        enriched_value: parsed_line["enrichedValue"]
+      )
+
+      enrich_doi(enrichment, doi)
+
+      unless doi.valid?
+        errors = serialize_errors(doi.errors, uid: enrichment.doi)
+        Rails.logger.error("#{log_prefix}: Enrichment does not generate valid metadata: #{errors}")
+        next
+      end
+
+      unless enrichment.save
+        errors = enrichment.errors.full_messages.join(";")
+        Rails.logger.error("#{log_prefix}: Enrichment failed to save: #{errors}")
+      end
+    end
+  end
+
+  def enrich_doi(enrichment, doi)
+    action = enrichment["action"]
+    case action
+    when "insert"
+      doi[enrichment["field"]] ||= []
+      doi[enrichment["field"]] << enrichment["enriched_value"]
+    when "update"
+      doi[enrichment["field"]] = enrichment["enriched_value"]
+    when "update_child"
+      field = enrichment["field"]
+      doi[field].each_with_index do |item, index|
+        if item == enrichment["original_value"]
+          doi[field][index] = enrichment["enriched_value"]
+        end
+      end
+    when "delete_child"
+      field = enrichment["field"]
+      doi[field] ||= []
+      doi[field].each_with_index do |item, index|
+        if item == enrichment["original_value"]
+          doi[field].delete_at(index)
+          break
+        end
+      end
     end
   end
 end
