fe0c20cc679b62f43f42a5388e884a323441695d
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-02-16 14:33:29 +0200

    Some refactoring

--- app/controllers/enrichments_controller.rb
+++ app/controllers/enrichments_controller.rb
@@ -8,26 +8,8 @@ class EnrichmentsController < ApplicationController
     client_id = params["client_id"]
     cursor = params["cursor"]
 
-    enrichments = if doi.present?
-      Enrichment.by_doi(doi)
-    elsif client_id.present?
-      Enrichment.by_client(client_id)
-    else
-      Enrichment.all
-    end
-
-    if cursor.present?
-      begin
-        decoded_cursor = decode_cursor(cursor)
-        cursor_updated_at = Time.iso8601(decoded_cursor.fetch("updated_at"))
-        cursor_id = decoded_cursor.fetch("id").to_i
-      rescue
-        raise ActionController::BadRequest, "Invalid cursor"
-      end
-
-      enrichments = enrichments.by_cursor(cursor_updated_at, cursor_id)
-    end
-
+    enrichments = base_page_enrichments(doi, client_id)
+    enrichments = cursor.present? ? filter_enrichments_with_cursor(enrichments, cursor) : enrichments
     enrichments = enrichments.order_by_cursor.limit(PAGE_SIZE).to_a
 
     current_link = request.original_url
@@ -37,9 +19,7 @@ class EnrichmentsController < ApplicationController
       encode_cursor(updated_at: last.updated_at.iso8601(6), id: last.id)
     end
 
-    next_link = doi.present? ?
-      "#{request.original_url.split("?").first}?doi=#{doi}&cursor=#{next_cursor}" :
-      "#{request.original_url.split("?").first}?client-id=#{client_id}&cursor=#{next_cursor}"
+    next_link = build_next_link(doi, client_id, next_cursor)
 
     render(json: {
       data: enrichments,
@@ -51,6 +31,28 @@ class EnrichmentsController < ApplicationController
   end
 
   private
+    def base_page_enrichments(doi, client_id)
+      enrichments = if doi.present?
+        Enrichment.by_doi(doi)
+      elsif client_id.present?
+        Enrichment.by_client(client_id)
+      else
+        Enrichment.all
+      end
+    end
+
+    def filter_enrichments_with_cursor(enrichments, cursor)
+      begin
+        decoded_cursor = decode_cursor(cursor)
+        cursor_updated_at = Time.iso8601(decoded_cursor.fetch("updated_at"))
+        cursor_id = decoded_cursor.fetch("id").to_i
+      rescue ArgumentError, KeyError
+        raise ActionController::BadRequest, "Invalid cursor"
+      end
+
+      enrichments.by_cursor(cursor_updated_at, cursor_id)
+    end
+
     def encode_cursor(hash)
       Base64.urlsafe_encode64(hash.to_json, padding: false)
     rescue
@@ -59,5 +61,19 @@ class EnrichmentsController < ApplicationController
 
     def decode_cursor(token)
       JSON.parse(Base64.urlsafe_decode64(token))
+    end
+
+    def build_next_link(doi, client_id, next_cursor)
+      base_link = request.original_url.split("?").first
+
+      query_string = if doi.present?
+        "doi=#{doi}&cursor=#{next_cursor}"
+      elsif client_id.present?
+        "client-id=#{client_id}&cursor=#{next_cursor}"
+      else
+        "cursor=#{next_cursor}"
+      end
+
+      "#{base_link}?#{query_string}"
     end
 end
--- app/jobs/enrichment_batch_process_job.rb
+++ app/jobs/enrichment_batch_process_job.rb
@@ -9,47 +9,56 @@ class EnrichmentBatchProcessJob < ApplicationJob
     log_prefix = "EnrichmentBatchProcessJob (#{file_name})"
 
     # We will process the lines in parallel to speed up ingestion.
-    Parallel.each(lines, in_threads: 10) do |line|
-      parsed_line = JSON.parse(line)
+    Parallel.each(lines, in_threads: 5) do |line|
+      # with_connection ensures the connection is explicitly checked out and returned to the pool after
+      # each iteration, preventing connection pool exhaustion.
+      ActiveRecord::Base.connection_pool.with_connection do
+        begin
+          parsed_line = JSON.parse(line)
+        rescue JSON::ParserError => e
+          Rails.logger.error("#{log_prefix}: Failed to parse line: #{e.message}")
+          next
+        end
 
-      # We only create enrichments for DOIs that exist and which have an agency of 'datacite'.
-      doi = Doi.find_by(doi: parsed_line["doi"], agency: "datacite")
+        # We only create enrichments for DOIs that exist and which have an agency of 'datacite'.
+        doi = Doi.find_by(doi: parsed_line["doi"], agency: "datacite")
 
-      if doi.blank?
-        Rails.logger.error("#{log_prefix}: Doi #{parsed_line["doi"]} does not exist")
-        next
-      end
+        if doi.blank?
+          Rails.logger.error("#{log_prefix}: Doi #{parsed_line["doi"]} does not exist")
+          next
+        end
 
-      if doi.enrichment_field(parsed_line["field"]).nil?
-        Rails.logger.error("#{log_prefix}: Unsupported enrichment field #{parsed_line["field"]} for DOI #{parsed_line["doi"]}")
-        next
-      end
+        if doi.enrichment_field(parsed_line["field"]).nil?
+          Rails.logger.error("#{log_prefix}: Unsupported enrichment field #{parsed_line["field"]} for DOI #{parsed_line["doi"]}")
+          next
+        end
 
-      # We set the only_validate flag on the DOI model to true such that we
-      # ensure that validation functions as expected when not persisting the record.
-      doi.only_validate = true
+        # We set the only_validate flag on the DOI model to true such that we
+        # ensure that validation functions as expected when not persisting the record.
+        doi.only_validate = true
 
-      enrichment = Enrichment.new(
-        doi: "#{parsed_line["doi"]}",
-        contributors: parsed_line["contributors"],
-        resources: parsed_line["resources"],
-        field: parsed_line["field"],
-        action: parsed_line["action"],
-        original_value: parsed_line["originalValue"],
-        enriched_value: parsed_line["enrichedValue"]
-      )
+        enrichment = Enrichment.new(
+          doi: "#{parsed_line["doi"]}",
+          contributors: parsed_line["contributors"],
+          resources: parsed_line["resources"],
+          field: parsed_line["field"],
+          action: parsed_line["action"],
+          original_value: parsed_line["originalValue"],
+          enriched_value: parsed_line["enrichedValue"]
+        )
 
-      doi.apply_enrichment(enrichment)
+        doi.apply_enrichment(enrichment)
 
-      unless doi.valid?
-        errors = serialize_errors(doi.errors, uid: enrichment.doi)
-        Rails.logger.error("#{log_prefix}: Enrichment does not generate valid metadata: #{errors}")
-        next
-      end
+        unless doi.valid?
+          errors = serialize_errors(doi.errors, uid: enrichment.doi)
+          Rails.logger.error("#{log_prefix}: Enrichment does not generate valid metadata: #{errors}")
+          next
+        end
 
-      unless enrichment.save
-        errors = enrichment.errors.full_messages.join(";")
-        Rails.logger.error("#{log_prefix}: Enrichment failed to save: #{errors}")
+        unless enrichment.save
+          errors = enrichment.errors.full_messages.join(";")
+          Rails.logger.error("#{log_prefix}: Enrichment failed to save: #{errors}")
+        end
       end
     end
   end
