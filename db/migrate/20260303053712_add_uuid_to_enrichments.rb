100099986f0146757f1c43f55a58a1a77ca439e0
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-03-03 12:35:07 +0200

    Remove null

--- db/migrate/20260303053712_add_uuid_to_enrichments.rb
+++ db/migrate/20260303053712_add_uuid_to_enrichments.rb
@@ -1,6 +1,6 @@
 class AddUuidToEnrichments < ActiveRecord::Migration[7.2]
   def change
-    add_column :enrichments, :uuid, :string, limit: 36, null: false
+    add_column :enrichments, :uuid, :string, limit: 36
 
     add_index :enrichments, :uuid, unique: true
   end
