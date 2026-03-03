a526e5ae63247cea60b0f0654ef104d7af5f0c72
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-03-03 10:41:19 +0200

    Rework uuid migration

--- db/migrate/20260303053712_add_uuid_to_enrichments.rb
+++ db/migrate/20260303053712_add_uuid_to_enrichments.rb
@@ -1,24 +1,7 @@
 class AddUuidToEnrichments < ActiveRecord::Migration[7.2]
   def change
-    add_column :enrichments, :uuid, :text
+    add_column :enrichments, :uuid, :string, limit: 36, null: false
 
-    migration_enrichment = Class.new(ActiveRecord::Base) do
-      self.table_name = "enrichments"
-    end
-
-    migration_enrichment.reset_column_information
-
-    migration_enrichment.where(uuid: nil).in_batches(of: 1000) do |relation|
-      relation.each { |row| row.update_columns(uuid: SecureRandom.uuid) }
-    end
-
-    change_column_null :enrichments, :uuid, false
-
-    add_index :enrichments, :uuid, unique: true, length: 36
-  end
-
-  def down
-    remove_index :enrichments, :uuid
-    remove_column :enrichments, :uuid
+    add_index :enrichments, :uuid, unique: true
   end
 end
