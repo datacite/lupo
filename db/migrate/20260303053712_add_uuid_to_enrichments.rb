9b6fba34bf8b6a0209cdf50515040232dfe84d08
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-03-03 13:13:17 +0200

    Fix migration

--- db/migrate/20260303053712_add_uuid_to_enrichments.rb
+++ db/migrate/20260303053712_add_uuid_to_enrichments.rb
@@ -1,6 +1,8 @@
 class AddUuidToEnrichments < ActiveRecord::Migration[7.2]
+  disable_departure!
+
   def change
-    add_column :enrichments, :uuid, :string, limit: 36
+    add_column :enrichments, :uuid, :string, limit: 36, null: false
 
     add_index :enrichments, :uuid, unique: true
   end
