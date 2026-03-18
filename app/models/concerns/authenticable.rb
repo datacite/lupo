07097d22f61b3554717355d3ec4ef16e12e96dc5
codycooperross <50597551+codycooperross@users.noreply.github.com>
2026-03-18 14:00:08 -0400

    Downcase consortium id for comparison

--- app/models/concerns/authenticable.rb
+++ app/models/concerns/authenticable.rb
@@ -222,7 +222,7 @@ module Authenticable
       return false if %w[staff_admin staff_user].include?(user.role_id)
       if %w[consortium_admin].include?(user.role_id) &&
           user.provider_id.present? &&
-          user.provider_id.upcase == doi.provider.consortium_id
+          user.provider_id.downcase == doi.provider.consortium_id.downcase
         return false
       end
       if %w[provider_admin provider_user].include?(user.role_id) &&
