b419da889c4c1bc9f29c80cf98d2ff8ed1475d64
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-03-24 10:48:57 +0200

    Rework migration

--- db/migrate/20260323093101_add_index_to_enrichments_filename.rb
+++ db/migrate/20260323093101_add_index_to_enrichments_filename.rb
@@ -1,36 +1,7 @@
 class AddIndexToEnrichmentsFilename < ActiveRecord::Migration[7.2]
   disable_departure!
 
-  # INDEX_NAME = "index_enrichments_on_filename"
-
   def change
     add_index :enrichments, :filename, name: "index_enrichments_on_filename"
   end
-
-  # def up
-  #   if mysql?
-  #     execute <<~SQL
-  #       ALTER TABLE `enrichments`
-  #       ADD INDEX `#{INDEX_NAME}` (`filename`)
-  #     SQL
-  #   else
-  #     add_index :enrichments, :filename, name: INDEX_NAME
-  #   end
-  # end
-
-  # def down
-  #   if mysql?
-  #     execute <<~SQL
-  #       ALTER TABLE `enrichments`
-  #       DROP INDEX `#{INDEX_NAME}`
-  #     SQL
-  #   else
-  #     remove_index :enrichments, name: INDEX_NAME
-  #   end
-  # end
-
-  # private
-  #   def mysql?
-  #     ActiveRecord::Base.connection.adapter_name.to_s.downcase.include?("mysql")
-  #   end
 end
