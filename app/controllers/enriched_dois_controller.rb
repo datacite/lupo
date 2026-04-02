12a2a94cb96fbcdaaa380aef986830e488977004
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-04-02 10:33:37 +0200

    Fix up serialization

--- app/controllers/enriched_dois_controller.rb
+++ app/controllers/enriched_dois_controller.rb
@@ -4,15 +4,13 @@ class EnrichedDoisController < ApplicationController
 
   def show
     uid = params[:id].upcase
-    doi = Doi.find_by(doi: uid, agency: "datacite")
+    doi = Doi.includes(:enrichments).find_by(doi: uid, agency: "datacite")
 
     # Return 404 if the DOI does not exist
     raise ActiveRecord::RecordNotFound if doi.blank?
 
-    enrichments = Enrichment.where(doi: uid).order(updated_at: :desc)
-
     # Return 404 if there are no enrichments for the DOI
-    raise ActiveRecord::RecordNotFound if enrichments.empty?
+    raise ActiveRecord::RecordNotFound if doi.enrichments.empty?
 
     # Ensure validation works as expected when not persisting the record
     doi.only_validate = true
@@ -23,29 +21,17 @@ class EnrichedDoisController < ApplicationController
     # Ensure we use schema version 4 for validation
     doi.schema_version = "http://datacite.org/schema/kernel-4" if doi.schema_version == "http://datacite.org/schema/kernel-3"
 
-    # Collection of enrichments that were successfully applied to the DOI
-    applied_enrichments = []
-
-    enrichments.each do |enrichment|
+    doi.enrichments.each do |enrichment|
       begin
         doi.apply_enrichment(enrichment)
-        applied_enrichments << enrichment
       rescue => e
         next
       end
     end
 
-    # Return 404 if zero enrichments were applied
-    raise ActiveRecord::RecordNotFound if applied_enrichments.empty?
-
     # Return 404 if the DOI is invalid after applying enrichments.
     raise ActiveRecord::RecordNotFound if doi.invalid?
 
-    data = DataciteDoiSerializer.new(doi).serializable_hash
-    applied_enrichments_json = EnrichmentSerializer.new(applied_enrichments).serializable_hash
-    data["relationships"] ||= {}
-    data["relationships"]["enrichments"] = applied_enrichments_json
-
-    render(json: data, status: :ok)
+    render(json: EnrichedDoiSerializer.new(doi).serializable_hash, status: :ok)
   end
 end
