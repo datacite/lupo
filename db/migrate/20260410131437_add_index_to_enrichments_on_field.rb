8916fe1f5b2a5adc6523ddc095017959e48c69d4
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-04-13 10:43:09 +0200

    Disable departure

--- db/migrate/20260410131437_add_index_to_enrichments_on_field.rb
+++ db/migrate/20260410131437_add_index_to_enrichments_on_field.rb
@@ -1,4 +1,6 @@
 class AddIndexToEnrichmentsOnField < ActiveRecord::Migration[7.2]
+  disable_departure!
+
   def change
     add_index :enrichments, :field, name: "index_enrichments_on_field"
   end
