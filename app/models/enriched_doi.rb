d6489e6e218a745082b8ff2e3ad120a17864b9d7
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-05-22 14:31:17 +0200

    Mate...

--- app/models/enriched_doi.rb
+++ app/models/enriched_doi.rb
@@ -469,29 +469,29 @@ class EnrichedDoi < Doi
     else
       ["dois", "enriched_dois"]
     end
+  end
 
-    def self.find_enriched_dois(dois)
-      return {} if dois.blank?
+  def self.find_enriched_dois(dois)
+    return {} if dois.blank?
 
-      response = __elasticsearch__.client.search(
-        index: index_name,
-        body: {
-          size: dois.length,
-          query: {
-            bool: {
-              filter: [
-                { terms: { doi: dois } }
-              ]
-            }
+    response = __elasticsearch__.client.search(
+      index: index_name,
+      body: {
+        size: dois.length,
+        query: {
+          bool: {
+            filter: [
+              { terms: { doi: dois } }
+            ]
           }
         }
-      )
+      }
+    )
 
-      (response.dig("hits", "hits") || []).each_with_object({}) do |hit, acc|
-        source = hit["_source"] || {}
-        doi = source["doi"]
-        acc[doi] = OpenStruct.new(source) if doi.present?
-      end
+    (response.dig("hits", "hits") || []).each_with_object({}) do |hit, acc|
+      source = hit["_source"] || {}
+      doi = source["doi"]
+      acc[doi] = OpenStruct.new(source) if doi.present?
     end
   end
 end
