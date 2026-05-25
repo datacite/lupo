fa8d744db56e4fc101aa8e2871d572f2ed9ec574
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-05-25 15:36:21 +0200

    Fix spec

--- app/models/enriched_doi.rb
+++ app/models/enriched_doi.rb
@@ -462,9 +462,7 @@ class EnrichedDoi < Doi
   end
 
   def self.search_indices
-    if Rails.env.test?
-      ["dois-test#{ENV['TEST_ENV_NUMBER']}", "enriched_dois-test#{ENV['TEST_ENV_NUMBER']}"]
-    elsif ENV["ES_PREFIX"].present?
+    if ENV["ES_PREFIX"].present?
       ["dois-#{ENV['ES_PREFIX']}", "enriched_dois-#{ENV['ES_PREFIX']}"]
     else
       ["dois", "enriched_dois"]
