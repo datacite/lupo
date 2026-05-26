e63aaf0e91728d57d8130099445130c2acc5979d
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2026-05-26 10:27:48 +0200

    Linting fixes

--- app/models/enriched_doi.rb
+++ app/models/enriched_doi.rb
@@ -468,7 +468,7 @@ class EnrichedDoi < Doi
 
   def self.search_indices
     if Rails.env.test?
-      ["dois-test#{ENV['TEST_ENV_NUMBER']}","enriched_dois-test#{ENV['TEST_ENV_NUMBER']}"]
+      ["dois-test#{ENV['TEST_ENV_NUMBER']}", "enriched_dois-test#{ENV['TEST_ENV_NUMBER']}"]
     elsif ENV["ES_PREFIX"].present?
       ["dois-#{ENV['ES_PREFIX']}", "enriched_dois-#{ENV['ES_PREFIX']}"]
     else
