02bdb009b517d219c0ddca7dbf9da1184e41c4c4
Suzanne Vogt <suzanne.vogt@datacite.org>
2026-06-19 12:01:26 -0400

    update

--- app/models/concerns/indexable.rb
+++ app/models/concerns/indexable.rb
@@ -50,13 +50,13 @@ module Indexable
       elsif instance_of?(Provider) && !from_salesforce && (Rails.env.production? || ENV["SQS_PREFIX"] == "stage")
         puts "****Indexable - SENDING EXPORT MESSAGE FOR PROVIDER #{@contact.provider.symbol}"
         puts data.inspect
-        send_provider_export_message(to_jsonapi.merge(slack_output: true))
+        # send_provider_export_message(to_jsonapi.merge(slack_output: true))
       elsif instance_of?(Client) && !from_salesforce && (Rails.env.production? || ENV["SQS_PREFIX"] == "stage")
         send_client_export_message(to_jsonapi.merge(slack_output: true))
       elsif instance_of?(Contact) && !from_salesforce && (Rails.env.production? || ENV["SQS_PREFIX"] == "stage")
         puts "****Indexable - SENDING EXPORT MESSAGE FOR CONTACT #{data[:email]}"
         puts data.inspect
-        send_contact_export_message(to_jsonapi.merge(slack_output: true))
+        # send_contact_export_message(to_jsonapi.merge(slack_output: true))
       end
     end
 
