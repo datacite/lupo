a93ff393083c88c34d614dae2cf57ca11efc44b9
jrhoads <jrhoads@datacite.org>
2026-06-29 20:45:19 +0200

    refactor: replace DISABLE_LEGACY_REST with sunset-based deprecation

--- app/controllers/concerns/legacy_rest_deprecation.rb
+++ app/controllers/concerns/legacy_rest_deprecation.rb
@@ -3,13 +3,30 @@
 module LegacyRestDeprecation
   extend ActiveSupport::Concern
 
-  LEGACY_REST_SUNSET = "Wed, 01 Jul 2026 00:00:00 GMT"
-
   included do
-    before_action :reject_if_legacy_rest_disabled, only: %i[index show]
+    before_action :reject_if_past_legacy_sunset, only: %i[index show]
+    after_action :set_legacy_sunset_headers, only: %i[index show]
+
+    rescue_from ActiveRecord::RecordNotFound, with: :legacy_render_not_found
   end
 
   class_methods do
+    def legacy_sunset_at(time)
+      @legacy_sunset_at = time
+    end
+
+    def legacy_sunset_at_value
+      @legacy_sunset_at
+    end
+
+    def legacy_sunset_link(url)
+      @legacy_sunset_link = url
+    end
+
+    def legacy_sunset_link_value
+      @legacy_sunset_link
+    end
+
     def legacy_replacement(path)
       @legacy_replacement_path = path
     end
@@ -20,8 +37,10 @@ module LegacyRestDeprecation
   end
 
   private
-    def reject_if_legacy_rest_disabled
-      return unless legacy_rest_disabled?
+    def reject_if_past_legacy_sunset
+      return unless past_legacy_sunset?
+
+      set_legacy_sunset_link_header
 
       replacement = self.class.legacy_replacement_path
       detail =
@@ -30,8 +49,6 @@ module LegacyRestDeprecation
         else
           "This endpoint has been deprecated. See the DataCite REST API documentation for supported endpoints."
         end
-
-      response.headers["Sunset"] = LEGACY_REST_SUNSET
 
       render json: {
         errors: [{
@@ -43,7 +60,36 @@ module LegacyRestDeprecation
              status: :gone
     end
 
-    def legacy_rest_disabled?
-      ENV["DISABLE_LEGACY_REST"].to_s.downcase.in?(%w[true 1])
+    def set_legacy_sunset_headers
+      return if past_legacy_sunset?
+
+      response.headers["Sunset"] = self.class.legacy_sunset_at_value.httpdate
+      set_legacy_sunset_link_header
+    end
+
+    def set_legacy_sunset_link_header
+      url = self.class.legacy_sunset_link_value
+      return if url.blank?
+
+      response.headers["Link"] = %(<#{url}>; rel="sunset")
+    end
+
+    def legacy_render_not_found(_exception)
+      set_legacy_sunset_headers unless past_legacy_sunset?
+
+      render json: {
+        errors: [{
+          status: "404",
+          title: "The resource you are looking for doesn't exist.",
+        }],
+      }.to_json,
+             status: :not_found
+    end
+
+    def past_legacy_sunset?
+      sunset_at = self.class.legacy_sunset_at_value
+      return false if sunset_at.blank?
+
+      Time.current.utc >= sunset_at
     end
 end
