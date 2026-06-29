f6dbf9ae2aafc6c7914f4b784a95aa405cd82195
Suzanne Vogt <suzanne.vogt@datacite.org>
2026-06-28 23:30:31 -0400

    Update providers_controller, exporting contacts first on create, then export provider.

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -375,14 +375,6 @@ class ProvidersController < ApplicationController
           end
         end
 
-        # Send provider export message.
-        @provider.save
-        @provider.send_provider_export_message(@provider.to_jsonapi.merge(slack_output: true)) if !@provider.from_salesforce && (Rails.env.production? || ENV["SQS_PREFIX"] == "stage")
-        puts "++++++++Saved provider #{@provider.symbol}"
-        puts "**Sent PROVIDER export FOR #{@provider.symbol}"
-        puts @provider.to_jsonapi.merge(slack_output: true)
-        puts JSON.pretty_generate(@provider.as_json)
-
         # Send contact export messages.
         contacts.each do |contact|
           contact.save
@@ -395,6 +387,15 @@ class ProvidersController < ApplicationController
           puts contact.to_jsonapi.merge(slack_output: true)
           puts "------------------------------------------------------"
         end
+
+        # Send provider export message.
+        @provider.save
+        @provider.send_provider_export_message(@provider.to_jsonapi.merge(slack_output: true)) if !@provider.from_salesforce && (Rails.env.production? || ENV["SQS_PREFIX"] == "stage")
+        puts "++++++++Saved provider #{@provider.symbol}"
+        puts "**Sent PROVIDER export FOR #{@provider.symbol}"
+        puts @provider.to_jsonapi.merge(slack_output: true)
+        puts JSON.pretty_generate(@provider.as_json)
+
       end
     end
 
